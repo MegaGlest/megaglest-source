9f4f9d10ea23ec71dc1fb871d1c0119171793e07
Titus Tscharntke <titi_ger@users.sourceforge.net>
2010-01-22 01:48:05 +0000

    initial version ( megaglest 3.2.3-beta3 )

---
+++ mk/linux/makerelease.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VERSION=`autoconf -t AC_INIT | sed -e 's/[^:]*:[^:]*:[^:]*:[^:]*:\([^:]*\):.*/\1/g'`
+RELEASENAME=megaglest-source
+RELEASEDIR="`pwd`/release/$RELEASENAME-$VERSION"
+
+echo "Creating source package in $RELEASEDIR"
+
+rm -rf $RELEASEDIR
+mkdir -p $RELEASEDIR
+# copy sources
+pushd "`pwd`/../../source"
+find glest_game/ \( -name "*.cpp" -o -name "*.h" \) -exec cp -p --parents "{}" $RELEASEDIR ';'
+find shared_lib/ \( -name "*.cpp" -o -name "*.h" \) -exec cp -p --parents "{}" $RELEASEDIR ';'
+find glest_map_editor/ \( -name "*.cpp" -o -name "*.h" \) -exec cp -p --parents "{}" $RELEASEDIR ';'
+popd
+AUTOCONFSTUFF="configure.ac autogen.sh Jamrules Jamfile `find mk/jam -name "*.jam"` `find mk/autoconf -name "*.m4" -o -name "config.*" -o -name "*sh"`"
+
+cp -p --parents $AUTOCONFSTUFF $RELEASEDIR
+cp -p ../../docs/readme*.txt ../../docs/*license*.txt $RELEASEDIR
+cp -p glest.ini $RELEASEDIR
+
+pushd $RELEASEDIR
+./autogen.sh
+popd
+
+pushd release
+PACKAGE="$RELEASENAME-$VERSION.tar.bz2"
+echo "creating $PACKAGE"
+tar -c --bzip2 -f "$PACKAGE" "$RELEASENAME-$VERSION"
+popd
