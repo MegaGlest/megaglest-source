c4a9f618423e54d8e8972524fe199a649c5858ac
Titus Tscharntke <titi_ger@users.sourceforge.net>
2010-03-19 18:58:46 +0000

    Bugfix for lua. Units died by hunger are reported in luas <unitDied>-section  too Also fixed the stats by counting this kind of death too.

--- source/glest_game/world/world.cpp
+++ source/glest_game/world/world.cpp
@@ -1,7 +1,7 @@
 // ==============================================================
 //	This file is part of Glest (www.glest.org)
 //
-//	Copyright (C) 2001-2008 Martiño Figueroa
+//	Copyright (C) 2001-2008 Martiï¿½o Figueroa
 //
 //	You can redistribute this code and/or modify it under
 //	the terms of the GNU General Public License as published
@@ -554,7 +554,7 @@ void World::initFactionTypes(GameSettings *gs){
 	for(int i=0; i<factions.size(); ++i){
 		const FactionType *ft= techTree.getType(gs->getFactionTypeName(i));
 		factions[i].init(
-			ft, gs->getFactionControl(i), &techTree, i, gs->getTeam(i),
+			ft, gs->getFactionControl(i), &techTree, game, i, gs->getTeam(i),
 			gs->getStartLocationIndex(i), i==thisFactionIndex, gs->getDefaultResources());
 
 		stats.setTeam(i, gs->getTeam(i));
