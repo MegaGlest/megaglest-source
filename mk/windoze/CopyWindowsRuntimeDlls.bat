e91c15b9e2adffbffc667e94ec13ac2b26ee4fa5
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-03-24 10:12:08 +0000

    Updates to the Win32 projects to link in zlib, libpng, and libjpeg

--- mk/windoze/CopyWindowsRuntimeDlls.bat
+++ mk/windoze/CopyWindowsRuntimeDlls.bat
@@ -2,5 +2,7 @@
 
 if not exist ..\..\data\glest_game\dsound.dll copy dsound.dll ..\..\data\glest_game\
 if not exist ..\..\data\glest_game\xerces-c_3_0.dll copy xerces-c_3_0.dll ..\..\data\glest_game\
+if not exist ..\..\data\glest_game\libpng14.dll copy libpng14.dll ..\..\data\glest_game\
+if not exist ..\..\data\glest_game\zlib1.dll copy zlib1.dll ..\..\data\glest_game\
 
 pause
