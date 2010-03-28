dab5cb840f8c77c23182f8079f029b4875f8b2b7
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-03-28 04:04:44 +0000

    Added a script for debian based linux distros to pull down required dependencies to build mega-glest

---
+++ mk/linux/setupBuildDeps.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt-get install build-essential subversion automake autoconf autogen jam
+sudo apt-get install libsdl1.2-dev libxerces-c2-dev libalut-dev libgl1-mesa-dev libglu1-mesa-dev libvorbis-dev libwxbase2.8-dev libwxgtk2.8-dev libx11-dev liblua5.1-0-dev
