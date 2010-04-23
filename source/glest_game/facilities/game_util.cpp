a0fbe84054a645766d16185f5630355106ba2a75
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-04-23 14:14:47 +0000

    Updated version # for last minor release of 3.3.4

--- source/glest_game/facilities/game_util.cpp
+++ source/glest_game/facilities/game_util.cpp
@@ -25,7 +25,7 @@ using namespace Shared::Platform;
 namespace Glest{ namespace Game{
 
 const string mailString= "contact_game@glest.org";
-const string glestVersionString= "v3.3.4.2";
+const string glestVersionString= "v3.3.4.3";
 
 string getCrashDumpFileName(){
 	return "glest"+glestVersionString+".dmp";
