6ec9c88b995f699fc28fa206bde98ad46dd352b9
Titus Tscharntke <titi_ger@users.sourceforge.net>
2010-05-12 22:44:41 +0000

    getCompileDateTime

--- source/glest_game/facilities/game_util.h
+++ source/glest_game/facilities/game_util.h
@@ -33,6 +33,7 @@ string getAboutString1(int i);
 string getAboutString2(int i);
 string getTeammateName(int i);
 string getTeammateRole(int i);
+string getCompileDateTime();
 
 string formatString(const string &str);
 
