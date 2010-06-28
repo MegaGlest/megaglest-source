d5a11ccca40ef48d477885a729a69fd7b8c5707b
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-06-28 00:25:12 +0000

    - small code rollback to see if it really affects render performance?

--- source/glest_game/graphics/renderer.cpp
+++ source/glest_game/graphics/renderer.cpp
@@ -458,7 +458,10 @@ void Renderer::renderParticleManager(ResourceScope rs){
 }
 
 void Renderer::swapBuffers(){
+
 	//glFlush(); // should not be required - http://www.opengl.org/wiki/Common_Mistakes
+	glFlush(); // <--- this might actually be needed for better Render performance???
+
 	GraphicsInterface::getInstance().getCurrentContext()->swapBuffers();
 }
 
