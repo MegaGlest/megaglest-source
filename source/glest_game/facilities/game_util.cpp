b0abff0721d7658563aba1cf4fd4f9149d3818fb
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-06-30 15:19:46 +0000

    - lets show Martiño and company the respect they deserve! Finally fixed the special spanish characters that eclipse was encoding as UTF-8 which messed up the display in about screen, in-game. File is now saved as ISO-8859-1 format to keep proper formatting.

--- source/glest_game/facilities/game_util.cpp
+++ source/glest_game/facilities/game_util.cpp
@@ -1,7 +1,7 @@
 // ==============================================================
 //	This file is part of Glest (www.glest.org)
 //
-//	Copyright (C) 2001-2008 Martiï¿½o Figueroa
+//	Copyright (C) 2001-2008 Martiño Figueroa
 //
 //	You can redistribute this code and/or modify it under
 //	the terms of the GNU General Public License as published
@@ -116,11 +116,11 @@ string getAboutString2(int i){
 
 string getTeammateName(int i){
 	switch(i){
-	case 0: return "MartiÃ±o Figueroa";
-	case 1: return "JosÃ© Luis GonzÃ¡lez";
-	case 2: return "Tucho  FernÃ¡ndez";
-	case 3: return "JosÃ© Zanni";
-	case 4: return "FÃ©lix MenÃ©ndez";
+	case 0: return "Martiño Figueroa";
+	case 1: return "José Luis González";
+	case 2: return "Tucho Fernández";
+	case 3: return "José Zanni";
+	case 4: return "Félix Menéndez";
 	case 5: return "Marcos Caruncho";
 	case 6: return "Matthias Braun";
 	case 7: return "Titus Tscharntke";
