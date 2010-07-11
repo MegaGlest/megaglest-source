c1414a1e7c1e1bb0867bf4fc455a7d0507fea602
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-07-11 15:56:00 +0000

    - fixed file encoding for proper credits display

--- source/glest_game/facilities/game_util.cpp
+++ source/glest_game/facilities/game_util.cpp
@@ -116,11 +116,11 @@ string getAboutString2(int i){
 
 string getTeammateName(int i){
 	switch(i){
-	case 0: return "Martio Figueroa";
-	case 1: return "Jos Luis Gonzlez";
-	case 2: return "Tucho Fernndez";
-	case 3: return "Jos Zanni";
-	case 4: return "Flix Menndez";
+	case 0: return "Martiño Figueroa";
+	case 1: return "José Luis González";
+	case 2: return "Tucho Fernández";
+	case 3: return "José Zanni";
+	case 4: return "Félix Menéndez";
 	case 5: return "Marcos Caruncho";
 	case 6: return "Matthias Braun";
 	case 7: return "Titus Tscharntke";
