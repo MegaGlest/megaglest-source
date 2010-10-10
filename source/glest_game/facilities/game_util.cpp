36f380abad6c7f011386c13dca1b39c77b6840e1
Titus Tscharntke <titi_ger@users.sourceforge.net>
2010-10-10 00:05:14 +0000

    version ist nicht mehr 3.3.7! es ist wieder development! 3.3.8-dev!

--- source/glest_game/facilities/game_util.cpp
+++ source/glest_game/facilities/game_util.cpp
@@ -1,7 +1,7 @@
 // ==============================================================
 //	This file is part of Glest (www.glest.org)
 //
-//	Copyright (C) 2001-2008 Martiño Figueroa
+//	Copyright (C) 2001-2008 Martiï¿½o Figueroa
 //
 //	You can redistribute this code and/or modify it under
 //	the terms of the GNU General Public License as published
@@ -26,7 +26,7 @@ using namespace Shared::Platform;
 namespace Glest { namespace Game {
 
 const string mailString			= "contact_game@glest.org";
-const string glestVersionString = "v3.3.7";
+const string glestVersionString = "v3.3.8-dev";
 const string SVN_Rev 			= "$Rev$";
 
 string getCrashDumpFileName(){
@@ -124,11 +124,11 @@ string getAboutString2(int i){
 
 string getTeammateName(int i) {
 	switch(i) {
-	case 0: return "Martiño Figueroa";
-	case 1: return "José Luis González";
-	case 2: return "Tucho Fernández";
-	case 3: return "José Zanni";
-	case 4: return "Félix Menéndez";
+	case 0: return "Martiï¿½o Figueroa";
+	case 1: return "Josï¿½ Luis Gonzï¿½lez";
+	case 2: return "Tucho Fernï¿½ndez";
+	case 3: return "Josï¿½ Zanni";
+	case 4: return "Fï¿½lix Menï¿½ndez";
 	case 5: return "Marcos Caruncho";
 	case 6: return "Matthias Braun";
 	case 7: return "Titus Tscharntke";
