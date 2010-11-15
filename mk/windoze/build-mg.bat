914649b647a7e89198c09bf7957187f6164c2fd1
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-11-15 23:58:29 +0000

    - added a batchfile that updates from svn and builds mg on commandline for windows

---
+++ mk/windoze/build-mg.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem change to the directory of this batch file
+ECHO --------------------------------
+ECHO Changing to build folder [%~dp0]
+cd /d "%~dp0"
+
+rem setup the Visual Studio 2008 environment
+ECHO --------------------------------
+ECHO Setting up Visual Studio 2008 environment vars...
+call "c:\Program Files\Microsoft Visual Studio 9.0\vc\vcvarsall.bat"
+
+rem Update from SVN to latest rev
+ECHO --------------------------------
+Echo Updating Code from SVN to latest Revision...
+svn update ..\..\
+
+rem Build Mega-Glest in release mode
+ECHO --------------------------------
+Echo Building Mega-Glest...
+msbuild Glest.sln /p:Configuration=Release
+
+rem pause execution so we can see the output before the batch file exits
+pause
