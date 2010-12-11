653f45e1624b5e1c278dcd4265e2d3f61912a55a
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-12-11 06:56:19 +0000

    - added CMAKE support for building MG in Windows using MingW

---
+++ build-mg.bat
@@ -0,0 +1,10 @@
+@echo off
+mkdir build
+cd build
+
+rem set build_verbose="-D CMAKE_VERBOSE_MAKEFILE=ON"
+set build_verbose=
+cmake -G "MinGW Makefiles" %build_verbose% -D wxWidgets_ROOT_DIR="..\source\win32_deps\wxWidgets-2.8.10" -D wxWidgets_LIB_DIR="..\source\win32_deps\wxWidgets-2.8.10\lib" -D wxWidgets_CONFIGURATION="mswu" ..
+
+mingw32-make
+cd ..\
