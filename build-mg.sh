8a14e826d5e32ffb88afbe03285674bffb5172dd
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-12-11 07:24:03 +0000

    - added a build script for Linux Cmake builds

---
+++ build-mg.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir build
+cd build
+cmake ..
+make
+
+echo 'You may now launch mega-glest from this folder like this:'
+echo '../mk/linux/glest.bin --ini-path=../mk/linux/ --data-path=../mk/linux/'
+
+
