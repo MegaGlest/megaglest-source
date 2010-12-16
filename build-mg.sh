807f26aaa1aafc20378ee97ed044f15e53a68978
mnaumann <mnaumann@users.sourceforge.net>
2010-12-16 10:52:34 +0000

    Corrections to linux build script, thanks cygal

--- build-mg.sh
+++ build-mg.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-mkdir build
+mkdir -p build
 cd build
 
 cmake ..
 make
 
-echo 'You may now launch mega-glest from this folder like this:'
-echo '../mk/linux/glest.bin --ini-path=../mk/linux/ --data-path=../mk/linux/'
-
-
+cd ..
+echo 'You may now launch mega-glest from this directory like this:'
+echo 'mk/linux/glest.bin --ini-path=mk/linux/ --data-path=mk/linux/'
