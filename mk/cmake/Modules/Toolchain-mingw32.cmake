f51ef741b9b72b563ded04ac9caa925a341eb201
Mark Vejvoda <mark_vejvoda@hotmail.com>
2010-12-17 02:19:46 +0000

    - checked in cross tool chain file

---
+++ mk/cmake/Modules/Toolchain-mingw32.cmake
@@ -0,0 +1,24 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
+SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc ${PROJECT_SOURCE_DIR}/source/win32_deps/lib)
+
+# which compilers to use for C and C++
+#SET(CMAKE_C_COMPILER amd64-mingw32msvc-gcc)
+#SET(CMAKE_CXX_COMPILER amd64-mingw32msvc-g++)
+
+# here is the target environment located
+#SET(CMAKE_FIND_ROOT_PATH  /usr/amd64-mingw32msvc ${PROJECT_SOURCE_DIR}/source/win32_deps/lib)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
