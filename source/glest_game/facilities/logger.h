741e264f89a2e8ae53b8f96829390b8cc651c66a
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-01-09 10:03:33 +0000

    - added game loading progress bar

--- source/glest_game/facilities/logger.h
+++ source/glest_game/facilities/logger.h
@@ -3,9 +3,9 @@
 //
 //	Copyright (C) 2001-2008 Marti�o Figueroa
 //
-//	You can redistribute this code and/or modify it under 
-//	the terms of the GNU General Public License as published 
-//	by the Free Software Foundation; either version 2 of the 
+//	You can redistribute this code and/or modify it under
+//	the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the
 //	License, or (at your option) any later version
 // ==============================================================
 
@@ -25,7 +25,7 @@ using Shared::Graphics::Texture2D;
 namespace Glest{ namespace Game{
 
 // =====================================================
-//	class Logger  
+//	class Logger
 //
 /// Interface to write log files
 // =====================================================
@@ -43,6 +43,7 @@ private:
 	string subtitle;
 	string current;
 	Texture2D *loadingTexture;
+	int progress;
 
 private:
 	Logger();
@@ -52,10 +53,11 @@ private:
 
 public:
 	static Logger & getInstance();
-	
+
 	void setFile(const string &fileName)		{this->fileName= fileName;}
 	void setState(const string &state)			{this->state= state;}
 	void setSubtitle(const string &subtitle)	{this->subtitle= subtitle;}
+	void setProgress(int value)                 { this->progress = value; }
 
 	void add(const string &str, bool renderScreen= false);
 	void loadLoadingScreen(string filepath);
