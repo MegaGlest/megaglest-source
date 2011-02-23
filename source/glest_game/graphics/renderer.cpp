8d16522874483a06c95e336920da0aefed1c6619
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-02-23 21:54:51 +0000

    - bugfix for selection circle on units on the edge of map cells

--- source/glest_game/graphics/renderer.cpp
+++ source/glest_game/graphics/renderer.cpp
@@ -2660,6 +2660,13 @@ void Renderer::renderSelectionEffects() {
 				}
 				else {
 					Vec2i pos= c->getPos();
+					if(pos.x < 0) {
+						pos.x = 0;
+					}
+					if(pos.y < 0) {
+						pos.y = 0;
+					}
+
 					arrowTarget= Vec3f(pos.x, map->getCell(pos)->getHeight(), pos.y);
 				}
 
@@ -2670,6 +2677,13 @@ void Renderer::renderSelectionEffects() {
 		//meeting point arrow
 		if(unit->getType()->getMeetingPoint()) {
 			Vec2i pos= unit->getMeetingPos();
+			if(pos.x < 0) {
+				pos.x = 0;
+			}
+			if(pos.y < 0) {
+				pos.y = 0;
+			}
+
 			Vec3f arrowTarget= Vec3f(pos.x, map->getCell(pos)->getHeight(), pos.y);
 			renderArrow(unit->getCurrVectorFlat(), arrowTarget, Vec3f(0.f, 0.f, 1.f), 0.3f);
 		}
