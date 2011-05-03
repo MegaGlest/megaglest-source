d7d8d0989200b060c20a859d1d0016399bc810af
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-05-03 06:28:59 +0000

    - removed windows dll from svn as it is available in win32 deps

--- mk/windoze/CopyWindowsRuntimeDlls.bat
+++ mk/windoze/CopyWindowsRuntimeDlls.bat
@@ -1,8 +1,6 @@
 @echo off
 
 rem if not exist ..\..\data\glest_game\dsound.dll copy dsound.dll ..\..\data\glest_game\
-if not exist ..\..\data\glest_game\xerces-c_3_0.dll copy xerces-c_3_0.dll ..\..\data\glest_game\
-if not exist ..\..\data\glest_game\libpng14.dll copy libpng14.dll ..\..\data\glest_game\
-if not exist ..\..\data\glest_game\zlib1.dll copy zlib1.dll ..\..\data\glest_game\
+if not exist ..\..\data\glest_game\xerces-c_3_0.dll copy ..\..\source\win32_deps\bin\xerces-c_3_0.dll ..\..\data\glest_game\
 
 pause
