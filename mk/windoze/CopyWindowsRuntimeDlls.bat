0898da16d61eddcec450f0ca29485736283e8719
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-05-03 10:20:50 +0000

    - remove 7z.exe from svn and make of the build script for windows

--- mk/windoze/CopyWindowsRuntimeDlls.bat
+++ mk/windoze/CopyWindowsRuntimeDlls.bat
@@ -3,4 +3,4 @@
 rem if not exist ..\..\data\glest_game\dsound.dll copy dsound.dll ..\..\data\glest_game\
 if not exist ..\..\data\glest_game\xerces-c_3_0.dll copy ..\..\source\win32_deps\bin\xerces-c_3_0.dll ..\..\data\glest_game\
 
-pause
+if not "%1" == "nopause" pause
