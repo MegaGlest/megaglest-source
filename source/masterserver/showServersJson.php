807a50b98b9920c864ebf3e12bbcdb9569294c70
mnaumann <mnaumann@users.sourceforge.net>
2011-07-17 21:31:26 +0000

    * add JSON masterserver server list, thanks statler!

---
+++ source/masterserver/showServersJson.php
@@ -0,0 +1,25 @@
+<?php
+	define( 'INCLUSION_PERMITTED', true );
+	require_once( 'config.php' );
+	require_once( 'functions.php' );
+
+	define( 'DB_LINK', db_connect() );
+
+	// consider replacing this by a cron job
+	cleanupServerList();
+
+	$servers_in_db = mysql_query( 'SELECT * FROM glestserver ORDER BY status, (networkSlots - connectedClients) DESC, lasttime DESC;' );
+	$all_servers = array();
+	while ( $server = mysql_fetch_array( $servers_in_db ) )
+	{
+		array_push( $all_servers, $server );
+	}
+	unset( $servers_in_db );
+	unset( $server );
+
+	db_disconnect( DB_LINK );
+	unset( $linkid );
+
+	echo json_encode($all_servers);
+	unset( $all_servers );
+?>
