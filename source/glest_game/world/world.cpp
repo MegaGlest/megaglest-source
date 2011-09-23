6ad1a95f97c6d562d55c20d1ced985b228a9fdb6
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-09-23 19:19:09 +0000

    - disable grouped commands by default until i track the segfault

--- source/glest_game/world/world.cpp
+++ source/glest_game/world/world.cpp
@@ -281,7 +281,7 @@ void World::updateAllFactionUnits() {
 	// Prioritize grouped command units so closest units to target go first
 	// units
 	Config &config= Config::getInstance();
-	bool sortedUnitsAllowed = config.getBool("AllowGroupedUnitCommands","true");
+	bool sortedUnitsAllowed = config.getBool("AllowGroupedUnitCommands","false");
 
 	int factionCount = getFactionCount();
 	if(sortedUnitsAllowed == true) {
