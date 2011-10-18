979bb162c3690075ad97c57077a9df2cade278b6
mnaumann <mnaumann@users.sourceforge.net>
2011-10-18 18:29:20 +0000

    Add GLEW dev package to fedora/opensuse build deps

--- mk/linux/setupBuildDeps.sh
+++ mk/linux/setupBuildDeps.sh
@@ -14,10 +14,10 @@ fi
 
 if [ -f /etc/SuSE-release ]; then
   echo "=====> Using build deps for Open SuSE 11.2 and above..."
-  sudo zypper install libSDL-devel libxerces-c-devel MesaGLw-devel freeglut-devel libvorbis-devel wxGTK-devel lua-devel libjpeg-devel libpng14-devel libcurl-devel openal-soft-devel xorg-x11-libX11-d libxml2-devevel libircclient-dev
+  sudo zypper install libSDL-devel libxerces-c-devel MesaGLw-devel freeglut-devel libvorbis-devel wxGTK-devel lua-devel libjpeg-devel libpng14-devel libcurl-devel openal-soft-devel xorg-x11-libX11-d libxml2-devevel libircclient-dev glew-devel
 elif [ -f /etc/fedora-release ]; then
   echo "=====> Using build deps for fedora 13 and above..."
-  sudo yum install SDL-devel xerces-c-devel mesa-libGL-devel mesa-libGLU-devel libvorbis-devel wxBase wxGTK-devel lua-devel libjpeg-devel libpng-devel libcurl-devel openal-soft-devel libX11-devel libxml2-dev libircclient-dev
+  sudo yum install SDL-devel xerces-c-devel mesa-libGL-devel mesa-libGLU-devel libvorbis-devel wxBase wxGTK-devel lua-devel libjpeg-devel libpng-devel libcurl-devel openal-soft-devel libX11-devel libxml2-dev libircclient-dev glew-devel
 elif [ "`uname -r`" = $ubuntu804_32 ]; then
   echo "=====> Using build deps for old Ubuntu 8.04..."
   sudo apt-get install libsdl1.2-dev libxerces28-dev libalut-dev libgl1-mesa-dev libglu1-mesa-dev libvorbis-dev libwxbase2.8-dev libwxgtk2.8-dev libx11-dev liblua5.1-0-dev libjpeg-dev libpng12-dev libcurl4-gnutls-dev libxml2-dev libircclient-dev libglew-dev
