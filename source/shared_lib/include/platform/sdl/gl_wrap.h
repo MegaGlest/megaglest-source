dfc6caf41f5b55dafc19e43b93a5c7c99e5cf716
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-11-19 18:29:44 +0000

    does this fix the mac build? weltall?

--- source/shared_lib/include/platform/sdl/gl_wrap.h
+++ source/shared_lib/include/platform/sdl/gl_wrap.h
@@ -74,8 +74,8 @@ static int charSet = 1;
 #endif
 
 #if defined(__APPLE__)
-void inline createGlFontBitmaps(uint32 &base, const string &type, int size, int width, int charCount, FontMetrics &metrics) {}
-void inline createGlFontOutlines(uint32 &base, const string &type, int width, float depth, int charCount, FontMetrics &metrics){}
+void inline createGlFontBitmaps(uint32 &base, const string &type, int size, int width, int charCount, FontMetrics &metrics);
+void inline createGlFontOutlines(uint32 &base, const string &type, int width, float depth, int charCount, FontMetrics &metrics);
 #else
 void createGlFontBitmaps(uint32 &base, const string &type, int size, int width, int charCount, FontMetrics &metrics);
 void createGlFontOutlines(uint32 &base, const string &type, int width, float depth, int charCount, FontMetrics &metrics);
