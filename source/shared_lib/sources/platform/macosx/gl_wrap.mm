2267c92748cc860d8ec2cd0dfb7a7a3c1039b0a2
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-11-19 19:07:21 +0000

    trying to fix mac build

--- source/shared_lib/sources/platform/macosx/gl_wrap.mm
+++ source/shared_lib/sources/platform/macosx/gl_wrap.mm
@@ -180,7 +180,7 @@ BOOL makeGLDisplayListFirst(unichar first, int count, GLint base, NSFont *font, 
    return retval;
 }
 
-void createGlFontBitmaps(GLuint32 &base, const string &type, int size, int width,
+void createGlFontBitmaps(uint32 &base, const string &type, int size, int width,
 						 int charCount, Shared::Graphics::FontMetrics &metrics) {
 	
 	
@@ -199,7 +199,7 @@ void createGlFontBitmaps(GLuint32 &base, const string &type, int size, int width
 
 }
 
-void createGlFontOutlines(GLuint32 &base, const string &type, int width,
+void createGlFontOutlines(uint32 &base, const string &type, int width,
 						  float depth, int charCount, Shared::Graphics::FontMetrics &metrics) {
 	NOIMPL;
 }
