92ec24ea76f238c1fec55645370fbb749abd73d1
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-11-29 05:11:25 +0000

    updated version # to beta2 in prep for new builds

--- source/glest_game/facilities/game_util.cpp
+++ source/glest_game/facilities/game_util.cpp
@@ -27,7 +27,7 @@ using namespace Shared::Platform;
 namespace Glest { namespace Game {
 
 const char *mailString				= " http://bugs.megaglest.org";
-const string glestVersionString 	= "v3.5.3-dev";
+const string glestVersionString 	= "v3.5.3-beta2";
 #if defined(SVNVERSION)
 const string SVN_Rev 			= string("Rev: ") + string(SVNVERSION);
 #elif defined(SVNVERSIONHEADER)
