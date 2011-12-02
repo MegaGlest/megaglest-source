dfd74604be2475d3d0f43e27989c0670acc6a5f9
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-12-02 18:30:04 +0000

    - added ftgl to deps for suse and fedora

--- mk/linux/setupBuildDeps.sh
+++ mk/linux/setupBuildDeps.sh
@@ -18,10 +18,10 @@ fi
 
 if [ -f /etc/SuSE-release ]; then
   echo "=====> Using build deps for Open SuSE 11.2 and above..."
-  sudo zypper install libSDL-devel libxerces-c-devel MesaGLw-devel freeglut-devel libvorbis-devel wxGTK-devel lua-devel libjpeg-devel libpng14-devel libcurl-devel openal-soft-devel xorg-x11-libX11-d libxml2-devevel libircclient-dev glew-devel
+  sudo zypper install libSDL-devel libxerces-c-devel MesaGLw-devel freeglut-devel libvorbis-devel wxGTK-devel lua-devel libjpeg-devel libpng14-devel libcurl-devel openal-soft-devel xorg-x11-libX11-d libxml2-devevel libircclient-dev glew-devel ftgl-devel
 elif [ -f /etc/fedora-release ]; then
   echo "=====> Using build deps for fedora 13 and above..."
-  sudo yum install SDL-devel xerces-c-devel mesa-libGL-devel mesa-libGLU-devel libvorbis-devel wxBase wxGTK-devel lua-devel libjpeg-devel libpng-devel libcurl-devel openal-soft-devel libX11-devel libxml2-dev libircclient-dev glew-devel
+  sudo yum install SDL-devel xerces-c-devel mesa-libGL-devel mesa-libGLU-devel libvorbis-devel wxBase wxGTK-devel lua-devel libjpeg-devel libpng-devel libcurl-devel openal-soft-devel libX11-devel libxml2-dev libircclient-dev glew-devel ftgl-devel
 elif [ "$OSTYPE" = "i686" ] && [ "$version" = "Ubuntu" ]; then
   echo "=====> Using build deps for old Ubuntu 8.04..."
   sudo apt-get install libsdl1.2-dev libxerces28-dev libalut-dev libgl1-mesa-dev libglu1-mesa-dev libvorbis-dev libwxbase2.8-dev libwxgtk2.8-dev libx11-dev liblua5.1-0-dev libjpeg-dev libpng12-dev libcurl4-gnutls-dev libxml2-dev libircclient-dev libglew-dev
