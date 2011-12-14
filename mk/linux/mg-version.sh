eca4f1934f043999ef8b8ba89efa1f23032e5d2b
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-12-14 09:39:03 +0000

    - updated cmake builds scripts to not fail if help2man is not installed

--- mk/linux/mg-version.sh
+++ mk/linux/mg-version.sh
@@ -5,7 +5,8 @@
 # Copyright (c) 2011 Mark Vejvoda under GNU GPL v3.0+
 
 OLD_MG_VERSION=3.6.0.1
-MG_VERSION=3.6.1-dev
+#MG_VERSION=3.6.1-dev
+MG_VERSION=3.6.0.2
 
 if [ "$1" = "--oldversion" ]; then
   echo "$OLD_MG_VERSION"
