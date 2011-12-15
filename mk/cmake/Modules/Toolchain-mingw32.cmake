6ddde29141ed5a0747c5bc5caea8b6116eb4da3d
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-12-15 21:10:40 +0000

    - updated to correct issues with the latest mingw compiler and resource compiler, as well as to use older miniupnpc

--- mk/cmake/Modules/Toolchain-mingw32.cmake
+++ mk/cmake/Modules/Toolchain-mingw32.cmake
@@ -4,6 +4,7 @@ SET(CMAKE_SYSTEM_NAME Windows)
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
 SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc ${PROJECT_SOURCE_DIR}/source/win32_deps/lib)
