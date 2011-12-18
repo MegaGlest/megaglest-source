dbd1e03f1299d50359df2ad467c6a65b4fa95ce9
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-12-18 15:18:46 +0000

    - updated copyright for md5 source code from David Henry (with his permission). Info pasted into docs/COPYRIGHT.source_code.txt

--- source/shared_lib/include/graphics/md5/md5util.h
+++ source/shared_lib/include/graphics/md5/md5util.h
@@ -3,10 +3,32 @@
 //
 //	Copyright (C) 2011-  by Mark Vejvoda
 //
-//	You can redistribute this code and/or modify it under
-//	the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 3 of the
-//	License, or (at your option) any later version
+// This code is licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license.php
+//
+// Open Source Initiative OSI - The MIT License (MIT):Licensing
+//
+// The MIT License (MIT)
+// Copyright (c) <year> <copyright holders>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.//
+//
 // ==============================================================
 
 #ifndef __MD5Loader_H__
