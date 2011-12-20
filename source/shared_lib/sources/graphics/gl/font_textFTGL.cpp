50f4668aaee549217fe32ab8dad5ed99dcf106fd
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-12-20 07:39:07 +0000

    - updated copyright headers to the same as the rest of the main mg source for files i wrote.

--- source/shared_lib/sources/graphics/gl/font_textFTGL.cpp
+++ source/shared_lib/sources/graphics/gl/font_textFTGL.cpp
@@ -5,7 +5,7 @@
 //
 //	You can redistribute this code and/or modify it under
 //	the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 3 of the
+//	by the Free Software Foundation; either version 2 of the
 //	License, or (at your option) any later version
 // ==============================================================
 
