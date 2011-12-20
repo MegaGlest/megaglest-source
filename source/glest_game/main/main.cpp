eb1408550d6fc9c736cfdc751c1fa584de8ee1d4
Mark Vejvoda <mark_vejvoda@hotmail.com>
2011-12-20 07:00:23 +0000

    - more debian related changes for better manpages

--- source/glest_game/main/main.cpp
+++ source/glest_game/main/main.cpp
@@ -2535,12 +2535,13 @@ int glestMain(int argc, char** argv) {
 
 	if( haveSpecialOutputCommandLineOption == false ||
 		hasCommandArgument(argc, argv,GAME_ARGS[GAME_ARG_VERSION]) == true) {
+		printf("%s %s",extractFileFromDirectoryPath(argv[0]).c_str(),getNetworkPlatformFreeVersionString().c_str());
+		printf("\nCompiled using: %s on: %s",getCompilerNameString().c_str(),getCompileDateTime().c_str());
+		printf("\nSVN: [%s]",getSVNRevisionString().c_str());
 #ifdef USE_STREFLOP
 //#	define STREFLOP_NO_DENORMALS
 //	streflop_init<streflop::Simple>();
-	printf("%s, SVN: [%s], [STREFLOP]\n",getNetworkVersionString().c_str(),getSVNRevisionString().c_str());
-#else
-	printf("%s, SVN: [%s]\n",getNetworkVersionString().c_str(),getSVNRevisionString().c_str());
+	printf(" - using STREFLOP\n");
 #endif
 	}
 
