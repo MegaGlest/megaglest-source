0a897bef4890651f1e651abd4a4f375c86d6640d
Mark Vejvoda <mark_vejvoda@hotmail.com>
2012-01-06 18:49:33 +0000

    - added ability to specify scaled image size of screenshots in g3d viewer example: ./megaglest_g3dviewer --load-unit=techs/megapack/factions/romans/units/catapult,attack_skill --auto-screenshot=resize-1024x768

--- source/g3d_viewer/renderer.cpp
+++ source/g3d_viewer/renderer.cpp
@@ -483,7 +483,7 @@ void Renderer::setAlphaColor(float alpha) {
     //printf("#3.1 The framebuffer uses %d bit(s) per the alpha component\n", alpha_bits);
 }
 
-void Renderer::saveScreen(const string &path) {
+void Renderer::saveScreen(const string &path,std::pair<int,int> *overrideSize) {
 	Pixmap2D *pixmapScreenShot = new Pixmap2D(width, height, 4);
 	//glFinish();
 
@@ -493,6 +493,10 @@ void Renderer::saveScreen(const string &path) {
 		GL_RGBA, GL_UNSIGNED_BYTE, pixmapScreenShot->getPixels());
 
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	if(overrideSize != NULL && overrideSize->first > 0 && overrideSize->second > 0) {
+		pixmapScreenShot->Scale(GL_RGBA,overrideSize->first,overrideSize->second);
+	}
 
 	pixmapScreenShot->save(path);
 	delete pixmapScreenShot;
