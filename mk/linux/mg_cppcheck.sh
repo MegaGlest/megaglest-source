7a099a51c3cd7fa7b89d41110eaa480a13016be7
Mark Vejvoda <mark_vejvoda@hotmail.com>
2012-03-28 05:58:13 +0000

    - added script to run cppcheck on the code

---
+++ mk/linux/mg_cppcheck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Use this script to check MegaGlest Source Code for errors using cppcheck
+# ----------------------------------------------------------------------------
+# Written by Mark Vejvoda <mark_vejvoda@hotmail.com>
+# Copyright (c) 2011 Mark Vejvoda under GNU GPL v3.0+
+
+cppcheck ../../source/ -i ../../source/win32_deps -i ../../source/configurator -j 5 --enable=all 2> cppcheck.log
+
+echo "Results from cppcheck were written to cppcheck.log"
+
