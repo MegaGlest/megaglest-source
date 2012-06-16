6b8487f6f501c341ef549dde0ba54666f86ca503
Tom Reynolds <tomreyn@users.sourceforge.net>
2012-06-16 06:52:54 +0000

    Improved portuguese translation - thanks Victor J.! Remove duplicate sysinfo.sh Add hint on iavailable cmake options to build-mg.sh

--- build-mg.sh
+++ build-mg.sh
@@ -12,7 +12,9 @@ cd build
 if [ -f 'CMakeCache.txt' ]; then rm -f 'CMakeCache.txt'; fi
 
 # This is for regular developers and used by our installer
-cmake -DCMAKE_INSTALL_PREFIX= -DWANT_STATIC_LIBS=ON ..
+# For more cmake/build options refer to 
+# http://wiki.megaglest.org/Linux_Compiling#Building_using_CMake_by_Hand
+cmake -DCMAKE_INSTALL_PREFIX='' -DWANT_STATIC_LIBS=ON ..
 if [ $? -ne 0 ]; then 
   echo 'ERROR: CMAKE failed.' >&2; exit 1
 fi
