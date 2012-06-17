a215c3bbb9f4a02a859a5d4e280caf8f61ee8c2c
Tom Reynolds <tomreyn@users.sourceforge.net>
2012-06-17 13:27:36 +0000

    minor improvements to mk/linux/setupBuildDeps.sh property changes to text files (check out with native line endings) and mk/linux/setupBuildDeps.sh (make executable)

--- mk/linux/setupBuildDeps.sh
+++ mk/linux/setupBuildDeps.sh
@@ -4,12 +4,11 @@
 # Written by Mark Vejvoda <mark_vejvoda@hotmail.com>
 # Copyright (c) 2011 Mark Vejvoda under GNU GPL v3.0+
 
-ubuntu804_32="2.6.24-27-generic"
-OSTYPE=`uname -m`
-# version=$(lsb_release -i | awk '{print $3}')
-version=`lsb_release -i | awk '{print $3}'`
+uname_m=`uname -m`
+#version=`lsb_release -i | awk '{print $3}'`
+version=`lsb_release -i -s`
 
-echo "Os = [$OSTYPE] version [$version]"
+echo "Os = [$uname_m] version [$version]"
 
 if [ -f /etc/fedora-release ]; then
   sudo yum groupinstall "Development Tools"
@@ -26,10 +25,10 @@ if [ -f /etc/SuSE-release ]; then
 elif [ -f /etc/fedora-release ]; then
   echo "=====> Using build deps for fedora 13 and above..."
   sudo yum install SDL-devel xerces-c-devel mesa-libGL-devel mesa-libGLU-devel libvorbis-devel wxBase wxGTK-devel lua-devel libjpeg-devel libpng-devel libcurl-devel openal-soft-devel libX11-devel libxml2-dev libircclient-dev glew-devel ftgl-devel
-elif [ "$OSTYPE" = "i686" ] && [ "$version" = "Ubuntu" ]; then
+elif [ "$uname_m" = "i686" ] && [ "$version" = "Ubuntu" ] && [ "`which lsb_release`x" != "x"] && [ "`lsb_release -r -s`" = "8.04" ]; then
   echo "=====> Using build deps for old Ubuntu 8.04..."
   sudo apt-get install libsdl1.2-dev libxerces-c2-dev libalut-dev libgl1-mesa-dev libglu1-mesa-dev libvorbis-dev libwxbase2.8-dev libwxgtk2.8-dev libx11-dev liblua5.1-0-dev libjpeg-dev libpng12-dev libcurl4-gnutls-dev libxml2-dev libircclient-dev libglew-dev libluajit-5.1-dev libvlc-dev libftgl-dev libvlc-dev
-elif [ "$OSTYPE" = "x86_64" ]; then
+elif [ "$uname_m" = "x86_64" ]; then
   echo "=====> Using build deps for debian based 64 bit linux..."
   sudo apt-get install libsdl1.2-dev libxerces-c2-dev libalut-dev libgl1-mesa-dev libglu1-mesa-dev libvorbis-dev libwxbase2.8-dev libwxgtk2.8-dev libx11-dev liblua5.1-0-dev libjpeg-dev libpng12-dev libcurl4-gnutls-dev cmake-curses-gui libgtk2.0-dev libxml2-dev libircclient-dev libftgl-dev libglew-dev libluajit-5.1-dev libvlc-dev
 
@@ -38,4 +37,3 @@ else
   echo "=====> Using build deps for debian based 32 or 64 bit Linux..."
   sudo apt-get install libsdl1.2-dev libxerces-c2-dev libalut-dev libgl1-mesa-dev libglu1-mesa-dev libvorbis-dev libwxbase2.8-dev libwxgtk2.8-dev libx11-dev liblua5.1-0-dev libjpeg-dev libpng12-dev libcurl4-gnutls-dev cmake-curses-gui libgtk2.0-dev libxml2-dev libircclient-dev libftgl-dev libminiupnpc-dev libglew-dev librtmp-dev libluajit-5.1-dev libvlc-dev
 fi
-
