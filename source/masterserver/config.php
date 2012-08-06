af2e1fa6817c3cd75ac966f84dfdc2e286bac8d2
Tom Reynolds <tomreyn@users.sourceforge.net>
2012-08-06 00:15:32 +0000

    slightly improved HTML server list

--- source/masterserver/config.php
+++ source/masterserver/config.php
@@ -5,16 +5,21 @@
 
 	if ( !defined('INCLUSION_PERMITTED') || ( defined('INCLUSION_PERMITTED') && INCLUSION_PERMITTED !== true ) ) { die( 'This file must not be invoked directly.' ); }
 
+	define( 'PRODUCT_NAME',       'MegaGlest' );
+
 	define( 'MYSQL_HOST',         '127.0.0.1' );
 	define( 'MYSQL_DATABASE',     'glest' );
 	define( 'MYSQL_USER',         'root' );
 	define( 'MYSQL_PASSWORD',     'your_pwd' );
+
 	// Allow using persistent MYSQL database server connections
 	// This _can_ improve performance remarkably but _can_ also break stuff completely, so read up:
 	// http://php.net/manual/function.mysql-pconnect.php
 	// http://php.net/manual/features.persistent-connections.php
 	define( 'MYSQL_LINK_PERSIST', false );
+
 	// How many recently seen servers to store
 	define( 'MAX_RECENT_SERVERS', 5 );
-	define( 'DEFAULT_COUNTRY',     '?' );
+
+	define( 'DEFAULT_COUNTRY',    '??' );
 ?>
