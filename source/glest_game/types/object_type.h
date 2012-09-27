5e57921fef2492b05626e4a481f6cb7e272ca6aa
Mark Vejvoda <mark_vejvoda@hotmail.com>
2012-09-27 15:04:28 +0000

    - some speed optimizations in areas mentioned by perf tool

--- source/glest_game/types/object_type.h
+++ source/glest_game/types/object_type.h
@@ -66,13 +66,13 @@ public:
 	TilesetModelType* loadModel(const string &path, std::map<string,vector<pair<string, string> > > *loadedFileList=NULL,
 			string parentLoader="");
 
-	TilesetModelType *getTilesetModelType(int i)			{return modeltypes[i];}
-	int getModelCount() const		{return modeltypes.size();}
-	const Vec3f &getColor() const	{return color;} 
-	int getClass() const			{return objectClass;}
-	bool getWalkable() const		{return walkable;}
-	int getHeight() const			{return height;}
-	bool isATree() const			{return objectClass==tree1 || objectClass==tree2;}
+	inline TilesetModelType *getTilesetModelType(int i)			{return modeltypes[i];}
+	inline int getModelCount() const		{return modeltypes.size();}
+	inline const Vec3f &getColor() const	{return color;}
+	inline int getClass() const			{return objectClass;}
+	inline bool getWalkable() const		{return walkable;}
+	inline int getHeight() const			{return height;}
+	inline bool isATree() const			{return objectClass==tree1 || objectClass==tree2;}
 	void deletePixels();
 };
 
