b55b45428e32409f7ecc2ea77b99dd3a57e394de
Mark Vejvoda <mark_vejvoda@hotmail.com>
2012-10-07 08:58:42 +0000

    - updated tool projects to avoid vc++ sxs dependency - added new bat files in windoze folder which create a standalone binary and stand alone data archive

---
+++ mk/windoze/make-binary-archive.bat
@@ -0,0 +1,44 @@
+@echo off
+
+rem change to the directory of this batch file
+ECHO --------------------------------
+ECHO Changing to build folder [%~dp0] p1 [%1] p2 [%2]
+rem pause
+cd /d "%~dp0"
+
+ECHO Checking for windows binaries...
+
+set mg_version=
+for /f "tokens=2 delims= " %%i in ('..\..\data\glest_game\megaglest.exe --version') do call :mgver %%i
+goto got_ver
+
+:mgver
+rem echo *[%1%]*
+if "%mg_version%." == "." goto set_mg_ver
+goto exit_mg_ver
+
+:set_mg_ver
+set mg_version=%1%
+rem echo *1[%mg_version%]
+set mg_version=%mg_version:~1%
+rem echo *2[%mg_version%]
+
+:exit_mg_ver
+exit /B 0
+
+:got_ver
+echo [%mg_version%]
+
+set RELEASENAME=megaglest-binary-win32-i386
+set PACKAGE=%RELEASENAME%-%mg_version%.7z
+
+cd /d ..\..\data\glest_game\
+
+echo creating [%PACKAGE%] ...
+if exist "%PACKAGE%" del "%PACKAGE%"
+7z a -mmt -mx=9 -ms=on -mhc=on "%PACKAGE%" megaglest.exe megaglest_g3dviewer.exe megaglest_editor.exe xerces-c_3_0.dll libvlc.dll libvlccore.dll lua plugins 7z.exe 7z.dll xml2g.exe openal32.dll g2xml.exe glest.ini glestkeys.ini servers.ini
+
+dir "%PACKAGE%"
+cd /d "%~dp0"
+
+pause
---
+++ mk/windoze/make-data-archive.bat
@@ -0,0 +1,81 @@
+@echo off
+
+rem change to the directory of this batch file
+ECHO --------------------------------
+ECHO Changing to build folder [%~dp0] p1 [%1] p2 [%2]
+rem pause
+cd /d "%~dp0"
+
+set mg_version=
+for /f "tokens=2 delims= " %%i in ('..\..\data\glest_game\megaglest.exe --version') do call :mgver %%i
+goto got_ver
+
+:mgver
+rem echo *[%1%]*
+if "%mg_version%." == "." goto set_mg_ver
+goto exit_mg_ver
+
+:set_mg_ver
+set mg_version=%1%
+rem echo *1[%mg_version%]
+set mg_version=%mg_version:~1%
+rem echo *2[%mg_version%]
+
+:exit_mg_ver
+exit /B 0
+
+:got_ver
+echo [%mg_version%]
+
+set RELEASENAME=megaglest-standalone-data
+set PACKAGE=%RELEASENAME%-%mg_version%.7z
+set RELEASEDIR=release\%RELEASENAME%-%mg_version%
+set PROJDIR=..\..\
+
+goto make_archive
+
+echo Creating data package in [%RELEASEDIR%]
+
+if exist %RELEASEDIR% rd /s /q %RELEASEDIR%
+mkdir %RELEASEDIR%
+
+rem copy data
+echo copying data ...
+mkdir %RELEASEDIR%\data\
+svn export --force ..\..\data\glest_game\data %RELEASEDIR%\data\
+mkdir %RELEASEDIR%\docs\
+svn export --force ..\..\data\glest_game\docs %RELEASEDIR%\docs\
+svn export --force ..\..\data\glest_game\docs\CHANGELOG.txt %RELEASEDIR%\docs\CHANGELOG.txt
+svn export --force ..\..\data\glest_game\README.txt %RELEASEDIR%\docs\README.txt
+mkdir %RELEASEDIR%\maps\
+svn export --force ..\..\data\glest_game\maps %RELEASEDIR%\maps\
+mkdir %RELEASEDIR%\scenarios\
+svn export --force ..\..\data\glest_game\scenarios %RELEASEDIR%\scenarios\
+mkdir %RELEASEDIR%\techs\
+svn export --force ..\..\data\glest_game\techs %RELEASEDIR%\techs\
+mkdir %RELEASEDIR%\tilesets\
+svn export --force ..\..\data\glest_game\tilesets %RELEASEDIR%\tilesets\
+mkdir %RELEASEDIR%\tutorials\
+svn export --force ..\..\data\glest_game\tutorials %RELEASEDIR%\tutorials\
+
+rem special export for flag images
+mkdir %RELEASEDIR%\data\core\misc_textures\flags\
+svn export --force ..\..\source\masterserver\flags %RELEASEDIR%\data\core\misc_textures\flags\
+
+rem START
+rem remove embedded data
+rem rm -rf "%RELEASEDIR%\data\core\fonts"
+rem END
+:make_archive
+echo Current directory[%CD%]
+echo creating data archive: %PACKAGE%
+if exist release\%PACKAGE% del release\%PACKAGE%
+cd /d %RELEASEDIR%
+echo Current directory[%CD%]
+
+..\..\..\..\data\glest_game\7z.exe a -mmt -mx=9 -ms=on -mhc=on ..\%PACKAGE% *
+
+dir "..\%PACKAGE%"
+cd /d "%~dp0"
+
+pause
