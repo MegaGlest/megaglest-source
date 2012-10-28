c5d6c611b1b716fcd17c1872534859da75a91fd9
Mark Vejvoda <mark_vejvoda@hotmail.com>
2012-10-28 22:04:25 +0000

    - updated to copy vlc lua files

--- mk/windoze/CopyWindowsRuntimeDlls_2010.bat
+++ mk/windoze/CopyWindowsRuntimeDlls_2010.bat
@@ -9,5 +9,6 @@ copy ..\..\source\windows_deps\lib\ssleay32.dll ..\..\data\glest_game\
 if not exist ..\..\data\glest_game\libvlc.dll copy ..\..\source\windows_deps\lib\libvlc.dll ..\..\data\glest_game\
 if not exist ..\..\data\glest_game\libvlccore.dll copy ..\..\source\windows_deps\lib\libvlccore.dll ..\..\data\glest_game\
 if not exist ..\..\data\glest_game\plugins\nul xcopy ..\..\source\windows_deps\vlc-2.0.1\plugins ..\..\data\glest_game\plugins\ /s
+if not exist ..\..\data\glest_game\lua\nul xcopy ..\..\source\windows_deps\vlc-2.0.1\lua ..\..\data\glest_game\lua\ /s
 
 if not "%1" == "nopause" pause
