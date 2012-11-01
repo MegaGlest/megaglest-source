13e6096600545473d5e80aa2b106948fce9af2fd
Mark Vejvoda <mark_vejvoda@hotmail.com>
2012-11-01 00:20:17 +0000

    - added new byte order code

--- source/shared_lib/include/platform/common/byte_order.h
+++ source/shared_lib/include/platform/common/byte_order.h
@@ -1,3 +1,14 @@
+// ==============================================================
+//	This file is part of MegaGlest (www.megaglest.org)
+//
+//	Copyright (C) 2012 Mark Vejvoda
+//
+//	You can redistribute this code and/or modify it under
+//	the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the
+//	License, or (at your option) any later version
+// ==============================================================
+
 #ifndef BYTE_ORDER_H
 #define BYTE_ORDER_H
 
