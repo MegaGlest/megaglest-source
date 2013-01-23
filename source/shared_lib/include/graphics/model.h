344593f01663ba42835b3907aed7e973b4d115f3
Mark Vejvoda <mark_vejvoda@hotmail.com>
2013-01-23 22:34:22 +0000

    - attempt to speed up color picking

--- source/shared_lib/include/graphics/model.h
+++ source/shared_lib/include/graphics/model.h
@@ -251,7 +251,7 @@ public:
     BaseColorPickEntity();
     virtual ~BaseColorPickEntity() {}
 
-    static const int COLOR_COMPONENTS = 4;
+    static const int COLOR_COMPONENTS = 3;
 
     struct ColorPickStruct {
       unsigned char color[COLOR_COMPONENTS];
