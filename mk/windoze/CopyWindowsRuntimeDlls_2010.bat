0f109de15723ef49baa0db6967ca40a6e93ebae1
Mark Vejvoda <mark_vejvoda@hotmail.com>
2013-01-31 20:38:22 +0000

    - fix the 2010 bat file as the dll's are not required

--- mk/windoze/CopyWindowsRuntimeDlls_2010.bat
+++ mk/windoze/CopyWindowsRuntimeDlls_2010.bat
@@ -3,8 +3,8 @@
 rem if not exist ..\..\data\glest_game\dsound.dll copy dsound.dll ..\..\data\glest_game\
 rem if not exist ..\..\data\glest_game\xerces-c_3_0.dll copy ..\..\source\windows_deps\bin\xerces-c_3_0.dll ..\..\data\glest_game\
 copy ..\..\source\windows_deps\lib\openal32.dll ..\..\data\glest_game\
-copy ..\..\source\windows_deps\lib\libeay32.dll ..\..\data\glest_game\
-copy ..\..\source\windows_deps\lib\ssleay32.dll ..\..\data\glest_game\
+rem copy ..\..\source\windows_deps\lib\libeay32.dll ..\..\data\glest_game\
+rem copy ..\..\source\windows_deps\lib\ssleay32.dll ..\..\data\glest_game\
 
 if not exist ..\..\data\glest_game\libvlc.dll copy ..\..\source\windows_deps\lib\libvlc.dll ..\..\data\glest_game\
 if not exist ..\..\data\glest_game\libvlccore.dll copy ..\..\source\windows_deps\lib\libvlccore.dll ..\..\data\glest_game\
