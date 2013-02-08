d491a6ae964b10ebdb5a63f6846592a1e0a2b233
Tom Reynolds <tomreyn@users.sourceforge.net>
2013-02-08 19:27:19 +0000

    upgrade linux version info

--- mk/linux/mg-version.sh
+++ mk/linux/mg-version.sh
@@ -4,10 +4,9 @@
 # Written by Mark Vejvoda <mark_vejvoda@hotmail.com>
 # Copyright (c) 2011 Mark Vejvoda under GNU GPL v3.0+
 
-OLD_MG_VERSION=3.7.0
-OLD_MG_VERSION_BINARY=3.7.0
-#MG_VERSION=3.6.1-dev
-MG_VERSION=3.7.1
+OLD_MG_VERSION=3.7.1
+OLD_MG_VERSION_BINARY=3.7.1
+MG_VERSION=3.8.0-dev
 
 if [ "$1" = "--oldversion" ]; then
   echo "$OLD_MG_VERSION"
