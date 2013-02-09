cb30df57a6ebf2d379971eae0e7667031dfab10f
Mark Vejvoda <mark_vejvoda@hotmail.com>
2013-02-09 20:45:21 +0000

    - added better breakpad lib detection

--- build-mg.sh
+++ build-mg.sh
@@ -15,7 +15,11 @@ if [ -f 'CMakeCache.txt' ]; then rm -f 'CMakeCache.txt'; fi
 # This is for regular developers and used by our installer
 # For more cmake/build options refer to 
 # http://wiki.megaglest.org/Linux_Compiling#Building_using_CMake_by_Hand
-cmake -DCMAKE_INSTALL_PREFIX='' -DWANT_DEV_OUTPATH=ON -DWANT_STATIC_LIBS=ON ..
+# this script looks for google-breakpad in the main root folder, you may link to the real path using:
+# ln -s ../../google-breakpad/ google-breakpad
+
+CURRENTDIR="$(dirname $(readlink -f $0))"
+cmake -DCMAKE_INSTALL_PREFIX='' -DWANT_DEV_OUTPATH=ON -DWANT_STATIC_LIBS=ON -DBREAKPAD_ROOT=${CURRENTDIR}/../google-breakpad/ ..
 if [ $? -ne 0 ]; then 
   echo 'ERROR: CMAKE failed.' >&2; exit 1
 fi
@@ -29,6 +33,8 @@ cd ..
 echo ''
 echo 'BUILD COMPLETE.'
 echo ''
+echo 'To build with boogle-breakpad support pass the path to the library as follows:'
+echo 'cmake -DBREAKPAD_ROOT=/home/softcoder/Code/google-breakpad/'
 echo 'To launch MegaGlest from the current directory, use:'
 echo '  mk/linux/megaglest --ini-path=mk/linux/ --data-path=mk/linux/'
 echo 'Or change into mk/linux and run it from there:'
