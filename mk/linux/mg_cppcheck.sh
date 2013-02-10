fa5f6f407a719a935ef90565398a8291ba36e7d4
Tom Reynolds <tomreyn@users.sourceforge.net>
2013-02-10 23:38:00 +0000

    new translations, cppcheck script modification

--- mk/linux/mg_cppcheck.sh
+++ mk/linux/mg_cppcheck.sh
@@ -4,7 +4,11 @@
 # Written by Mark Vejvoda <mark_vejvoda@hotmail.com>
 # Copyright (c) 2011 Mark Vejvoda under GNU GPL v3.0+
 
-cppcheck ../../source/ -i ../../source/win32_deps -i ../../source/configurator -j 5 --enable=all --force --verbose 2> cppcheck.log
+OUTFILE=./cppcheck.log
 
-echo "Results from cppcheck were written to cppcheck.log"
+CPUS=`lscpu -p | grep -cv '^#'`
+if [ "$CPUS" = '' ]; then CPUS=1; fi
 
+cppcheck ../../source/ -i ../../source/win32_deps -i ../../source/configurator -j $CPUS --enable=all --force --verbose 2> $OUTFILE
+
+echo "Results from cppcheck were written to $OUTFILE"
