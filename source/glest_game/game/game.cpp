d041f1eea6e14d1e2698796dde6e2bd4fb6c8033
Titus Tscharntke <titi_ger@users.sourceforge.net>
2013-03-10 01:32:00 +0000

    commented my last changes ( beside "+1" )as itmade clients freeze on exiting games

--- source/glest_game/game/game.cpp
+++ source/glest_game/game/game.cpp
@@ -1741,11 +1741,11 @@ void Game::update() {
 
 					updateLoops += frameDifference;
 				}
-				//If client is ahead maybe this fixes it ( by titi ):
-				if(lastNetworkFrameFromServer > 0 && world.getFrameCount() > lastNetworkFrameFromServer && (world.getFrameCount()%gameSettings.getNetworkFramePeriod())==19 ){
-					printf("Client will slow down because no message has arrived yet. currentFrame=%d \n",world.getFrameCount());
-					updateLoops = 0;
-				}
+//				//If client is ahead maybe this fixes it ( by titi ):
+//				if(lastNetworkFrameFromServer > 0 && world.getFrameCount() > lastNetworkFrameFromServer && (world.getFrameCount()%gameSettings.getNetworkFramePeriod())==19 ){
+//					printf("Client will slow down because no message has arrived yet. currentFrame=%d \n",world.getFrameCount());
+//					updateLoops = 0;
+//				}
 			}
 		}
 
