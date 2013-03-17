ef58a2285dce36f5e3769858b18560df8b6c1aca
Tom Reynolds <tomreyn@users.sourceforge.net>
2013-03-17 18:55:59 +0000

    Changes to olaus' techtree HTML docs script: * rename mg.ini to megapack.ini * add Opensuse support (thanks atze)

--- source/tools/convert_faction_xml2html/setupDeps.sh
+++ source/tools/convert_faction_xml2html/setupDeps.sh
@@ -6,29 +6,28 @@
 # Rewritten by Tom Reynolds <tomreyn@megaglest.org>
 # Copyright (c) 2012 Mark Vejvoda, Tom Reynolds under GNU GPL v3.0
 
-OSTYPE=`uname -m`
-echo "Detected distro: [$OSTYPE]"
+echo 'Downloading Javascript libraries...'
+wget http://code.jquery.com/jquery-1.5.2.js -O media/jquery-1.5.min.js
+wget http://www.datatables.net/download/build/jquery.dataTables.min.js -O media/jquery.dataTables.min.js
 
-curl http://code.jquery.com/jquery-1.5.2.js > media/jquery-1.5.min.js
-curl http://www.datatables.net/download/build/jquery.dataTables.min.js > media/jquery.dataTables.min.js
-
+echo 'Installing dependencies...'
 # This should actually use /etc/issue
 if [ -f /etc/fedora-release ]; then
-  echo "Fedora..."
+  echo 'Fedora...'
   #sudo yum groupinstall "Development Tools"
   #sudo yum install subversion automake autoconf autogen jam
 elif [ -f /etc/SuSE-release ]; then
-  echo "SuSE..."
-  #sudo zypper install subversion gcc gcc-c++ automake
+  echo '(Open)SuSE...'
+  sudo zypper install graphviz-perl perl-GraphViz perl-Config-IniFiles perl-PerlMagick
 elif [ -f /etc/debian_version ]; then
-  echo "Debian / Ubuntu..."
+  echo 'Debian / Ubuntu...'
   sudo apt-get install perl graphviz libgraphviz-perl libconfig-inifiles-perl perlmagick
   #sudo apt-get install libimage-size-perl
 else
-  echo 'Unknown distribution. Stopping here.' >&2
+  echo 'ERROR: Unknown distribution. Stopping here.' >&2
   exit 1
 fi
 
-echo "To run the techtree html builder edit mg.ini and run the script as follows:"
-echo "./convert_faction_xml2html.pl mg.ini"
-
+echo ''
+echo 'To run the techtree html builder edit mg.ini and run the script as follows:'
+echo './convert_faction_xml2html.pl megapack.ini'
