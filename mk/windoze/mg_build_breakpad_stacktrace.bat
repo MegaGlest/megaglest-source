c09d807ad82f9cb0f7ff2cf568227a8a41dda189
Tom Reynolds <tomreyn@users.sourceforge.net>
2013-05-17 19:20:07 +0000

    some error handling for windows breakpad stacktrace dumping

--- mk/windoze/mg_build_breakpad_stacktrace.bat
+++ mk/windoze/mg_build_breakpad_stacktrace.bat
@@ -1,2 +1,17 @@
 @echo
-..\..\source\windows_deps\google-breakpad\trunk\src\minidump_stackwalk.exe "%AppData%\megaglest\%1" .\windows_symbols 
+
+if "%1." == "." goto NOTSET
+if NOT EXIST "%AppData%\megaglest\%1" goto NOTFOUND
+
+..\..\source\windows_deps\google-breakpad\trunk\src\minidump_stackwalk.exe "%AppData%\megaglest\%1" .\windows_symbols
+goto END
+
+:NOTSET
+echo "You need to pass the name of the DMP file (in %AppData%) to process as the first argument."
+goto END
+
+:NOTFOUND
+echo "File not found [%AppData%\megaglest\%1]"
+goto END
+
+:END
