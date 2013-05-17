7feb5435f8a5f3bed232e87e5d3b6f44bd8249ea
Mark Vejvoda <mark_vejvoda@hotmail.com>
2013-05-17 19:13:43 +0000

    updated windows script to use generic path variable

--- mk/windoze/mg_build_breakpad_stacktrace.bat
+++ mk/windoze/mg_build_breakpad_stacktrace.bat
@@ -1,2 +1,2 @@
 @echo
-..\..\source\windows_deps\google-breakpad\trunk\src\minidump_stackwalk.exe "c:\Documents and Settings\SoftCoder\Application Data\megaglest\%1" .\windows_symbols 
+..\..\source\windows_deps\google-breakpad\trunk\src\minidump_stackwalk.exe "%AppData%\megaglest\%1" .\windows_symbols 
