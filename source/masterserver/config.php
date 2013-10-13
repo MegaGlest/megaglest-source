6c18fbd81894fa694a55e7e04a6d1afcad3b8bba
Tom Reynolds <tomreyn@users.sourceforge.net>
2013-10-13 01:44:27 +0000

    Master server: * more white labeling * installation instructions added * updated database scheme * javascript refresh on HTML server list (thanks Omega!) * desktop notifications on HTML server list (thanks Omega!)

--- source/masterserver/config.php
+++ source/masterserver/config.php
@@ -9,8 +9,8 @@
 	define( 'PRODUCT_URL',        'http://megaglest.org' );
 
 	define( 'MYSQL_HOST',         '127.0.0.1' );
-	define( 'MYSQL_DATABASE',     'glest' );
-	define( 'MYSQL_USER',         'root' );
+	define( 'MYSQL_DATABASE',     'megaglest_master' );
+	define( 'MYSQL_USER',         'not_root' );
 	define( 'MYSQL_PASSWORD',     'your_pwd' );
 
 	// Allow using persistent MYSQL database server connections
