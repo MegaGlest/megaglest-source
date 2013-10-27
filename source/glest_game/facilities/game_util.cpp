92b58100ad1dc3803c25f982b79b1427d338696b
Titus Tscharntke <titi_ger@users.sourceforge.net>
2013-10-27 16:17:20 +0000

    changed version number to avoid beta1 people joining svn test games

--- source/glest_game/facilities/game_util.cpp
+++ source/glest_game/facilities/game_util.cpp
@@ -27,7 +27,7 @@ using namespace Shared::Platform;
 namespace Glest { namespace Game {
 
 const char *mailString				= " http://bugs.megaglest.org";
-const string glestVersionString 	= "v3.8.0-dev";
+const string glestVersionString 	= "v3.8-dev";
 #if defined(SVNVERSION)
 const string SVN_RawRev			= string(SVNVERSION);
 const string SVN_Rev 			= string("Rev: ") + string(SVNVERSION);
