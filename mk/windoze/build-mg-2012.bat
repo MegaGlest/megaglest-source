597feb452f11c27b9947c7614c8354ff01c442a1
Mark Vejvoda <mark_vejvoda@hotmail.com>
2013-11-10 05:27:59 +0000

    fix warnings from vc++

--- mk/windoze/build-mg-2012.bat
+++ mk/windoze/build-mg-2012.bat
@@ -6,6 +6,8 @@ rem C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110\Platforms\Win32\Platf
 rem C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110\Platforms\Win32\Microsoft.Cpp.Win32.Common.props to C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32
 
 rem change to the directory of this batch file
+SET VCVARS_PLATFORM=x86_amd64
+
 ECHO --------------------------------
 ECHO Changing to build folder [%~dp0] p1 ["%1"] p2 ["%2"]
 rem pause
@@ -62,17 +64,17 @@ rem IF EXIST "\Program Files (x86)\Microsoft Visual Studio 10.0\" GOTO VC_64
 goto SVNSECTION
 
 :VC_Common_12
-call "%VS110COMNTOOLS%..\..\vc\vcvarsall.bat" x86
+call "%VS110COMNTOOLS%..\..\vc\vcvarsall.bat" %VCVARS_PLATFORM%
 goto SVNSECTION
 
 :VC_32_12
 ECHO 32 bit Windows detected...
-call "\Program Files\Microsoft Visual Studio 11.0\vc\vcvarsall.bat"
+call "\Program Files\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" %VCVARS_PLATFORM%
 goto SVNSECTION
 
 :VC_64_12
 ECHO 64 bit Windows detected...
-call "\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat"
+call "\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" %VCVARS_PLATFORM%
 goto SVNSECTION
 
 :VC_Common
@@ -123,6 +125,7 @@ rem set PATH=%PROGRAM_FILES_X86%\Microsoft SDKs\Windows\v7.1A\Bin;%PATH%
 rem set LIB=%PROGRAM_FILES_X86%\Microsoft SDKs\Windows\v7.1A\Lib;%LIB%
 rem set CL=/D_USING_V110_SDK71_ %CL%
 rem set PlatformToolset=v110_xp
+rem set PlatformToolset=V110
 
 del ..\..\source\glest_game\facilities\svnversion.h
 
@@ -130,7 +133,7 @@ if not "%SVNVERSION%" == "." set CL=/DSVNVERSIONHEADER
 if not "%SVNVERSION%" == "."  echo building with CL [%CL%]
 if not "%SVNVERSION%" == "." echo #define SVNVERSION "%SVNVERSION%" > ..\..\source\glest_game\facilities\svnversion.h
 
-set VisualStudioVersion=11.0
+rem set VisualStudioVersion=11.0
 set msBuildMaxCPU=
 SET BuildInParallel=false
 if %NUMBER_OF_PROCESSORS% GTR 2 (
@@ -139,14 +142,17 @@ if %NUMBER_OF_PROCESSORS% GTR 2 (
 				SET msBuildMaxCPU=/maxcpucount)
 
 ECHO Found CPU Count [%NUMBER_OF_PROCESSORS%]
+SET MSBUILD_PATH_MG_x64="C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110\\"
+
 if "%2" == "rebuild" echo Doing a FULL REBUILD...
 rem if "%2" == "rebuild" msbuild /detailedsummary %msBuildMaxCPU% /p:BuildInParallel=%BuildInParallel% /p:Configuration=Release /t:Rebuild Glest_vc2010.sln
 rem if "%2" == "rebuild" msbuild %msBuildMaxCPU% /p:Configuration=Release;Platform=x64 /v:q /m /t:Rebuild /p:PlatformToolset=v110_xp;VisualStudioVersion=11.0 Glest_vc2012.sln
-if "%2" == "rebuild" msbuild %msBuildMaxCPU% /p:Configuration=Release;Platform=x64 /v:q /m /t:Rebuild /p:VisualStudioVersion=11.0 Glest_vc2012.sln
+rem /p:VCTargetsPath=%MSBUILD_PATH_MG_x64%;
+if "%2" == "rebuild" msbuild %msBuildMaxCPU% /v:detailed /p:VCTargetsPath=%MSBUILD_PATH_MG_x64%;Configuration=Release;Platform=x64;PlatformToolset=v110 /m /t:Rebuild Glest_vc2012.sln
 
 rem if not "%2" == "rebuild" msbuild /detailedsummary %msBuildMaxCPU% /p:BuildInParallel=%BuildInParallel% /p:Configuration=Release Glest_vc2010.sln
 rem if not "%2" == "rebuild" msbuild %msBuildMaxCPU% /p:Configuration=Release;Platform=x64 /v:q /m /p:PlatformToolset=v110_xp Glest_vc2012.sln
-if not "%2" == "rebuild" msbuild %msBuildMaxCPU% /p:Configuration=Release;Platform=x64 /v:q /m Glest_vc2012.sln
+if not "%2" == "rebuild" msbuild %msBuildMaxCPU% /v:detailed /p:VCTargetsPath=%MSBUILD_PATH_MG_x64%;Configuration=Release;Platform=x64;PlatformToolset=v110 /m Glest_vc2012.sln
 
 rem pause execution so we can see the output before the batch file exits
 if not "%1" == "nopause" pause
