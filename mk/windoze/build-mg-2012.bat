c97825d898f5d6de6179df4d1e81ed14b40b6014
SoftCoder <mark_vejvoda@hotmail.com>
2013-12-02 09:45:46 -0800

    - updated windows x64 build to use git revision stamp

--- mk/windoze/build-mg-2012.bat
+++ mk/windoze/build-mg-2012.bat
@@ -50,7 +50,7 @@ ECHO Setting up Visual Studio 2012 environment vars...
 REM Ensure ultifds HP doesn't mess the build up
 SET Platform=
 if "%DevEnvDir%." == "." goto SETVCVARS
-GOTO SVNSECTION
+GOTO GITSECTION
 
 :SETVCVARS
 
@@ -61,45 +61,50 @@ IF EXIST "\Program Files (x86)\Microsoft Visual Studio 11.0\" GOTO VC_64_12
 rem IF EXIST "%VS100COMNTOOLS%..\..\"                             GOTO VC_Common
 rem IF EXIST "\Program Files\Microsoft Visual Studio 10.0\"       GOTO VC_32
 rem IF EXIST "\Program Files (x86)\Microsoft Visual Studio 10.0\" GOTO VC_64
-goto SVNSECTION
+goto GITSECTION
 
 :VC_Common_12
 call "%VS110COMNTOOLS%..\..\vc\vcvarsall.bat" %VCVARS_PLATFORM%
-goto SVNSECTION
+goto GITSECTION
 
 :VC_32_12
 ECHO 32 bit Windows detected...
 call "\Program Files\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" %VCVARS_PLATFORM%
-goto SVNSECTION
+goto GITSECTION
 
 :VC_64_12
 ECHO 64 bit Windows detected...
 call "\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" %VCVARS_PLATFORM%
-goto SVNSECTION
+goto GITSECTION
 
 :VC_Common
 rem call "%VS100COMNTOOLS%..\..\vc\vcvarsall.bat"
-goto SVNSECTION
+goto GITSECTION
 
 :VC_32
 ECHO 32 bit Windows detected...
 rem call "\Program Files\Microsoft Visual Studio 10.0\vc\vcvarsall.bat"
-goto SVNSECTION
+goto GITSECTION
 
 :VC_64
 ECHO 64 bit Windows detected...
 rem call "\Program Files (x86)\Microsoft Visual Studio 10.0\vc\vcvarsall.bat"
-goto SVNSECTION
+goto GITSECTION
 
-:SVNSECTION
-rem Update from SVN to latest rev
+:GITSECTION
+rem Update from GIT to latest rev
 ECHO --------------------------------
-Echo Updating Code from SVN to latest Revision...
-svn update ..\..\
+Echo Updating Code from GIT to latest Revision...
+cd ..\..\
+git pull
 
-set SVNVERSION=.
-for /f "delims=" %%a in ('svnversion ..\..\ -n') do @set SVNVERSION=%%a
-ECHO Will build using SVN Revision: [%SVNVERSION%]
+set GITVERSION_SHA1=.
+set GITVERSION_REV=.
+set GET_GIT_SHA1="git log -1 --format=%%h"
+for /f "delims=" %%a in ('%GET_GIT_SHA1%') do @set GITVERSION_SHA1=%%a
+for /f "delims=" %%a in ('git rev-list HEAD --count') do @set GITVERSION_REV=%%a
+ECHO Will build using GIT Revision: [%GITVERSION_REV%.%GITVERSION_SHA1%]
+cd mk\windoze
 rem pause
 
 ECHO --------------------------------
@@ -127,12 +132,12 @@ rem set CL=/D_USING_V110_SDK71_ %CL%
 rem set PlatformToolset=v110_xp
 rem set PlatformToolset=V110
 
-del ..\..\source\glest_game\facilities\svnversion.h
+del ..\..\source\glest_game\facilities\gitversion.h
 
-if not "%SVNVERSION%" == "." set CL=/DSVNVERSIONHEADER
-if not "%SVNVERSION%" == "."  echo building with CL [%CL%]
-if not "%SVNVERSION%" == "." echo #define SVNVERSION "%SVNVERSION%" > ..\..\source\glest_game\facilities\svnversion.h
-if not "%SVNVERSION%" == "." copy /b ..\..\source\glest_game\facilities\game_util.cpp +,,
+if not "%GITVERSION_SHA1%" == "." set CL=/DGITVERSIONHEADER
+if not "%GITVERSION_SHA1%" == "."  echo building with CL [%CL%]
+if not "%GITVERSION_SHA1%" == "." echo #define GITVERSION "%GITVERSION_REV%.%GITVERSION_SHA1%" > ..\..\source\glest_game\facilities\gitversion.h
+if not "%GITVERSION_SHA1%" == "." copy /b ..\..\source\glest_game\facilities\game_util.cpp +,,
 
 rem set VisualStudioVersion=11.0
 set msBuildMaxCPU=
