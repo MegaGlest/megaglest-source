4d4b81d2a5c093e8f22e79d808f9985dd18084d6
SoftCoder <mark_vejvoda@hotmail.com>
2013-12-18 13:51:15 -0800

    - updated windows coverity script to read from settings file

--- mk/windoze/coverity-scan.bat
+++ mk/windoze/coverity-scan.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 rem
 rem Upload Coverity s
 rem Requires:
@@ -8,14 +8,33 @@ rem - ..\..\data\glest_game\7z.exe (should get installed automatically during a 
 rem - Coverity Scan Build Tool installed and in %PATH%
 rem
 
+rem Change into this directory
+cd /d "%~dp0"
+
 rem Project name (case sensitive)
 set PROJECT=MegaGlest
 
+rem read in config settings
+if not exist ".coverity-submit" (
+        echo -----------------------------------------
+        echo **Missing Config** To use this script please create a config file named [%CD%\.coverity-submit]
+        echo Containing: TOKEN=x , EMAIL=x , COVERITY_ANALYSIS_ROOT=x , NUMCORES=x
+        goto END
+)
+
+setlocal disabledelayedexpansion
+FOR /F "tokens=1* delims==" %%i IN (.coverity-submit) DO set "prop_%%i=%%j"
+
 rem Coverity Scan project token as listed on the Coverity Scan project page
-set TOKEN=FIXME
+set TOKEN=%prop_TOKEN%
 
 rem E-Mail address of registered Coverity Scan user with project access
-set EMAIL=FIXME
+set EMAIL=%prop_EMAIL%
+
+set COVERITY_ANALYSIS_ROOT=%prop_COVERITY_ANALYSIS_ROOT%
+
+echo TOKEN [%TOKEN%] EMAIL [%EMAIL%] COVERITY_ANALYSIS_ROOT [%COVERITY_ANALYSIS_ROOT%]
+rem pause
 
 rem Description of this build (can be any string)
 set DESCRIPTION=Windows-32_%COMPUTERNAME%
@@ -39,12 +58,19 @@ rem Untested! Requires modification.
 rem ..\..\data\glest_game\wget.exe --no-check-certificate https://scan.coverity.com/download/win-32 --post-data "token=%TOKEN%&project=%PROJECT%" -O %TEMP%\coverity_tool.zip
 rem ..\..\data\glest_game\7z.exe x %TEMP%\coverity_tool.zip
 rem set PATH=%PATH%;C:\build\megaglest-source\mk\windoze\cov-analysis-win32-6.6.1\bin\
-
+if "%MG_COV_PATH_SET%." == "." set PATH=%PATH%;%COVERITY_ANALYSIS_ROOT%\bin\
+set MG_COV_PATH_SET=TRUE
 
 cov-build --dir %BUILDTOOL% build-mg-2010.bat nopause rebuild
+if ERRORLEVEL 1 GOTO ERROR
+
 ..\..\data\glest_game\7z.exe a %FILENAME%.tar %BUILDTOOL%\
 ..\..\data\glest_game\7z.exe a %FILENAME%.tar.gz %FILENAME%.tar
 del /Q /F %FILENAME%.tar
+dir %FILENAME%.tar.gz
+
+Echo **About to run: ..\..\data\glest_game\curl.exe --insecure --form "project=%PROJECT%" --form "token=%TOKEN%" --form "email=%EMAIL%" --form "version=%VERSION%" --form "description=%DESCRIPTION%" --form "file=@%FILENAME%.tar.gz" https://scan5.coverity.com/cgi-bin/upload.py
+rem pause
 rem echo Running curl
 ..\..\data\glest_game\curl.exe --insecure --form "project=%PROJECT%" --form "token=%TOKEN%" --form "email=%EMAIL%" --form "version=%VERSION%" --form "description=%DESCRIPTION%" --form "file=@%FILENAME%.tar.gz" https://scan5.coverity.com/cgi-bin/upload.py
 if ERRORLEVEL 1 GOTO ERROR
