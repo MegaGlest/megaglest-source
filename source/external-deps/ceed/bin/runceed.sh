f7c5a16c005ee899841acc256747d7ca2efbb356
Tom Reynolds <tomreyn@users.noreply.github.com>
2014-02-15 19:16:13 +0100

    Make runceed.sh universal
    [skip ci]

--- source/external-deps/ceed/bin/runceed.sh
+++ source/external-deps/ceed/bin/runceed.sh
@@ -1,3 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-PYTHONPATH=../../cegui-source/build/lib:../:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../../cegui-v0-8/build/lib:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../../cegui/build/lib:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../../cegui/build/lib:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../../cegui/build/lib:/home/softcoder/Code/megaglest/git/source/external-deps/ceed/bin/../: python2 ./ceed-gui
+MYPATH=`dirname -z $0 | xargs --null readlink -f`
+echo "MYPATH: $MYPATH" >&2
+
+PATHS_IN_PYTHONPATH="\
+`readlink -f \"$MYPATH/../../cegui-source/build/lib/\"` \
+`readlink -f \"$MYPATH/../\"` \
+`readlink -f \"$MYPATH/../../cegui-v0-8/build/lib/\"` \
+`readlink -f \"$MYPATH/../../cegui/build/lib/\"` \
+"
+
+PYTHONPATH=''
+for THISPATH in $PATHS_IN_PYTHONPATH
+do
+  PYTHONPATH="$THISPATH:$PYTHONPATH"
+  echo "Adding PYTHONPATH component: $THISPATH" >&2
+done
+
+echo "Final PYTHONPATH:" >&2
+echo "$PYTHONPATH" >&2
+
+PYTHONPATH="$PYTHONPATH" python2 ./ceed-gui
