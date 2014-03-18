88f5a7d8d6fb27dcb742e8c6e92c8ac8b4231186
Sebastian Riedel <git@basxto.de>
2014-03-19 00:41:01 +0100

    documentation for Tile

--- source/glest_map_editor/tile.h
+++ source/glest_map_editor/tile.h
@@ -25,45 +25,68 @@ class QColor;
 class QString;
 
 namespace MapEditor {
-        class Renderer;
+    class Renderer;
 
-        class Tile:public QGraphicsItem{
-            public:
-                Tile(QGraphicsScene *scene, Renderer *renderer,int column=0,int row=0);
-                //~Tile();
-                void setSurface(int surface);
-                void setHeight(double height);
-                double getHeight() const;
-                void updateHeight();
-                void recalculate();
-                virtual QRectF boundingRect() const;
-                virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
-            protected:
-                virtual void mousePressEvent ( QGraphicsSceneMouseEvent * event);
-                virtual void mouseMoveEvent ( QGraphicsSceneMouseEvent * event);
-                virtual void mouseReleaseEvent ( QGraphicsSceneMouseEvent * event);
-                virtual void hoverEnterEvent ( QGraphicsSceneHoverEvent * event );
-                virtual void dragEnterEvent (QGraphicsSceneDragDropEvent * event);
-            private:
-                void move(int column, int row);
-                static const QColor SURFACE[];
-                static const QColor OBJECT[];
-                static const QColor RESOURCE[];
-                static const int SIZE;
-                Renderer* renderer;
-                bool topLine;
-                bool rightLine;
-                bool bottomLine;
-                bool leftLine;
-                double height;
-                int column;
-                int row;
-                QColor color;
-                bool water;
-                bool object;
-                QColor objectColor;
-                bool resource;
-                QColor resourceColor;
-        };
+    class Tile:public QGraphicsItem{
+        public:
+            Tile(QGraphicsScene *scene, Renderer *renderer,int column=0,int row=0);
+            //~Tile();
+            /**
+             * FIXME: critical, surfaces are enums
+             * @param surface 0-4
+             */
+            void setSurface(int surface);
+            /**
+             * altitude
+             */
+            void setHeight(double height);
+            /**
+             * altitude
+             */
+            double getHeight() const;
+            /**
+             * get height from map
+             */
+            void updateHeight();
+            /**
+             * Setting color, borders, objects and resources depending on map cell.
+             * Borders depend on the height of this tile and the height of the direct neighbors
+             */
+            void recalculate();
+            virtual QRectF boundingRect() const;
+            virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+        protected:
+            virtual void mousePressEvent ( QGraphicsSceneMouseEvent * event);
+            virtual void mouseMoveEvent ( QGraphicsSceneMouseEvent * event);
+             virtual void mouseReleaseEvent ( QGraphicsSceneMouseEvent * event);
+            virtual void hoverEnterEvent ( QGraphicsSceneHoverEvent * event );
+            virtual void dragEnterEvent (QGraphicsSceneDragDropEvent * event);
+        private:
+            /**
+             * Changes the position of this tile, does not use built in setPos.
+             * Uses an offset for bounding box and painter instead.
+             * @param column New position
+             * @param row New position
+             */
+            void move(int column, int row);
+            static const QColor SURFACE[];
+            static const QColor OBJECT[];
+            static const QColor RESOURCE[];
+            static const int SIZE;
+            Renderer* renderer;
+            bool topLine;
+            bool rightLine;
+            bool bottomLine;
+            bool leftLine;
+            double height;
+            int column;
+            int row;
+            QColor color;
+            bool water;
+            bool object;
+            QColor objectColor;
+            bool resource;
+            QColor resourceColor;
+    };
 }
 #endif
