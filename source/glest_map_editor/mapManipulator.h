3d19f4dc7397e13fd4981937c9174e1d74ea4f01
Sebastian Riedel <git@basxto.de>
2014-03-18 22:15:52 +0100

    documentation for MapManipulator

--- source/glest_map_editor/mapManipulator.h
+++ source/glest_map_editor/mapManipulator.h
@@ -1,13 +1,26 @@
-// ==============================================================
-//  This file is part of MegaGlest (www.megaglest.org)
-//
-//  Copyright (C) 2014 Sebastian Riedel
-//
-//  You can redistribute this code and/or modify it under
-//  the terms of the GNU General Public License as published
-//  by the Free Software Foundation; either version 2 of the
-//  License, or (at your option) any later version
-// ==============================================================
+/**
+ * @file mapManipulator.h
+ * @author  Sebastian Riedel
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * The map manipulator class glues the GUI and MapPreview.
+ * This file is part of MegaGlest (megaglest.org)
+ */
+
 #ifndef MAPMANIPULATOR_H
 #define MAPMANIPULATOR_H
 
@@ -26,9 +39,32 @@ namespace MapEditor{
             ~MapManipulator();
             void setRenderer(Renderer *renderer);
         public slots:
+            /**
+             * Sets the radius for the drawing tool
+             * @param *radius A QAction representing a radius size
+             */
             void setRadius(QAction *radius);
+            /**
+             * Draws with selected drawing tool and radius
+             * @param column Where you wanna draw
+             * @param row Where you wanna draw
+             */
             void changeTile(int column, int row);
+            /**
+             * Click which initiates drawing.
+             * Chooses the height for gradient and height tools
+             * @param column Where you wanna draw
+             * @param row Where you wanna draw
+             */
             void click(int column, int row);
+            /**
+             * Resets the actual map / creates a new one.
+             * @param width New size of the map
+             * @param height New size of the map
+             * @param surface Whole map gets filled with this surface
+             * @param altitude Whole map will have this altitude
+             * @param players Number of players for this map
+             */
             void reset(int width, int height, int surface, float altitude, int players);
         private:
             MainWindow *win;//for user input
