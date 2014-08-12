88501db8fc50c0ae5cd861a61a4aecaa36d887e7
Sebastian Riedel <git@basxto.de>
2014-08-12 19:07:59 +0200

    Revert "moving camera with GL_PROJECTION instead of GL_MODELVIEW"
    This reverts commit c4d4e6247b00df6101a278aaaf158f3a632af27e.

--- source/g3d_viewer/renderer.cpp
+++ source/g3d_viewer/renderer.cpp
@@ -133,8 +133,7 @@ Renderer * Renderer::getInstance() {
 void Renderer::transform(float rotX, float rotY, float zoom) {
         assertGl();
 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
+        glMatrixMode(GL_MODELVIEW);
         glRotatef(rotY, 1.0f, 0.0f, 0.0f);
         glRotatef(rotX, 0.0f, 1.0f, 0.0f);
         glScalef(zoom, zoom, zoom);
@@ -266,9 +265,9 @@ void Renderer::setDimension(int w, int h) {
         glMatrixMode(GL_PROJECTION);//??
         glLoadIdentity();//??
         gluPerspective(60.0f, static_cast<float>(width)/height, 1.0f, 200.0f);//??
-        //~ glMatrixMode(GL_MODELVIEW);//??
-        //~ glLoadIdentity();//??
-        //~ glTranslatef(0, -1.5, -5);//??
+        glMatrixMode(GL_MODELVIEW);//??
+        glLoadIdentity();//??
+        glTranslatef(0, -1.5, -5);//??
 }
 
 void Renderer::setPlayerColor(float red, float green, float blue) {
@@ -287,13 +286,19 @@ void Renderer::reset() {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //reset projection
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        //??
-        gluPerspective(60.0f, static_cast<float>(width)/height, 1.0f, 200.0f);
-        //default camera position
-        glTranslatef(0, -1.5, -5);
+        //~ if(wireframe) {//better do this in toggleWireframe?
+                //~ glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+                //~ glDisable(GL_TEXTURE_2D);
+                //~ glDisable(GL_LIGHTING);
+                //~ glDisable(GL_LIGHT0);
+        //~ }
+        //~ else {
+                //~ glEnable(GL_TEXTURE_2D);
+                //~ glEnable(GL_LIGHTING);
+                //~ glEnable(GL_LIGHT0);
+                //~ glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        //~ }
+
         assertGl();
 }
 
