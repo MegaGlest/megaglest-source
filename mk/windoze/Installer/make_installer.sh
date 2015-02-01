adb71b4add4a0e7c2337732bdd8cb61517ac7d1c
titiger <titi_linux@megaglest.org>
2015-02-02 00:17:05 +0100

    get rid of AccessControl.dll in repository
    this is now downloaded on demand.

--- mk/windoze/Installer/make_installer.sh
+++ mk/windoze/Installer/make_installer.sh
@@ -1,6 +1,16 @@
 #/bin/bash
+# This is a help script to build windows installers on a linux machine
+#
+# for this script nsis is needed:
+# windows binaries must be in place ( for example from snapshots.megaglest.org )
 
-# for this script nsis is needed
-# windows binaries must be installed from snapshots.megaglest.org
+CURRENTDIR="$(dirname $(readlink -f $0))"
+cd "$CURRENTDIR"
+
+# We need the AccessControl plugin for nsis and place this in plugins directory
+# original from http://nsis.sourceforge.net/AccessControl_plug-in
+wget http://downloads.megaglest.org/windowsInstallerHelp/AccessControl.dll -P plugins
+wget http://downloads.megaglest.org/windowsInstallerHelp/AccessControl.txt -P plugins
+
 makensis MegaGlestInstaller.nsi
 
