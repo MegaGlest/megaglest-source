3385220430aa553a8a3190af894151976550a221
titison <philipp@titusgames.de>
2015-09-21 19:07:07 +0200

    Chat/Console message appears when an upgrade finishes.

--- source/glest_game/type_instances/faction.cpp
+++ source/glest_game/type_instances/faction.cpp
@@ -887,6 +887,8 @@ void Faction::cancelUpgrade(const UpgradeType *ut){
 
 void Faction::finishUpgrade(const UpgradeType *ut){
 	upgradeManager.finishUpgrade(ut);
+	Console *console=world->getGame()->getConsole();
+	console->addStdMessage("UpgradeFinished",": " + formatString(ut->getName(true)));
 	for(int i=0; i<getUnitCount(); ++i){
 		getUnit(i)->applyUpgrade(ut);
 	}
