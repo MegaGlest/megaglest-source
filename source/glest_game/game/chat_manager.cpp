312a4e8a649b0cb34f89249339b6d1946dc30440
SoftCoder <mark_vejvoda@hotmail.com>
2015-11-09 13:22:50 -0800

    - try to fix backspace after doing a paste

--- source/glest_game/game/chat_manager.cpp
+++ source/glest_game/game/chat_manager.cpp
@@ -104,7 +104,9 @@ bool ChatManager::textInput(std::string inputText) {
 	string textToAdd=inputText.substr (0,maxpaste);
 	if(editEnabled && (int)text.length() < maxTextLenAllowed) {
 		SystemFlags::OutputDebug(SystemFlags::debugSystem,"In [%s::%s Line: %d]\n",__FILE__,__FUNCTION__,__LINE__);
-		textCharLength.push_back(textToAdd.length());
+		for(unsigned int idx = 0; idx < textToAdd.size(); ++idx) {
+		  textCharLength.push_back(1);
+		}
 		this->text +=textToAdd;
 		updateAutoCompleteBuffer();
 		return true;
