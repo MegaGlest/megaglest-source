a3c930b2da46f2c233de6326f1a5a5c1c6731420
Tom Reynolds <tomreyn@users.noreply.github.com>
2017-01-30 14:57:38 +0100

    Moderate changes to cppcheck script
    skipci

--- mk/linux/mg_cppcheck.sh
+++ mk/linux/mg_cppcheck.sh
@@ -4,12 +4,12 @@
 # Written by Mark Vejvoda <mark_vejvoda@hotmail.com>
 # Copyright (c) 2011 Mark Vejvoda under GNU GPL v3.0+
 
-OUTFILE=./cppcheck.log
+CPPCHECK=$(which cppcheck)
+LOGFILE=/tmp/cppcheck.log
 
 CPUS=`lscpu -p | grep -cv '^#'`
 if [ "$CPUS" = '' ]; then CPUS=1; fi
 
-#~/cppcheck-1.62/cppcheck ../../source/ -i ../../source/win32_deps -i ../../source/configurator -i ../../source/shared_lib/sources/libircclient -i ../../source/shared_lib/sources/platform/miniupnpc -j $CPUS --enable=all --force --verbose 2> $OUTFILE
-cppcheck ../../source/ -i ../../source/win32_deps -i ../../source/configurator -i ../../source/shared_lib/sources/libircclient -i ../../source/shared_lib/sources/platform/miniupnpc -i ../../source/shared_lib/sources/streflop -j $CPUS --enable=all --force --verbose 2> $OUTFILE
+$CPPCHECK ../../source/ -i ../../source/win32_deps -i ../../source/configurator -i ../../source/shared_lib/sources/libircclient -i ../../source/shared_lib/sources/platform/miniupnpc -i ../../source/shared_lib/sources/streflop -j $CPUS --enable=all --force --verbose 2> $LOGFILE
 
-echo "Results from cppcheck were written to $OUTFILE"
+echo "Results from cppcheck were written to $LOGFILE"
