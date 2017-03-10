438d37c59f7d22d70bceb39e35f20b5efcbec851
titiger <titi_linux@megaglest.org>
2017-03-10 01:14:27 +0100

    render meeting point for all selected units

--- source/glest_game/graphics/renderer.cpp
+++ source/glest_game/graphics/renderer.cpp
@@ -5678,6 +5678,15 @@ void Renderer::renderSelectionEffects(int healthbarMode) {
 					}
 				}
 			}
+
+			//meeting point arrow
+			if(unit->getType()->getMeetingPoint()) {
+				Vec2i pos= unit->getMeetingPos();
+				map->clampPos(pos);
+
+				Vec3f arrowTarget= Vec3f(pos.x, map->getCell(pos)->getHeight(), pos.y);
+				renderArrow(unit->getCurrVectorFlat(), arrowTarget, Vec3f(0.f, 0.f, 1.f), 0.3f);
+			}
 		}
 	}
 
@@ -5727,15 +5736,6 @@ void Renderer::renderSelectionEffects(int healthbarMode) {
 
 					renderArrow(unit->getCurrVectorFlat(), arrowTarget, arrowColor, 0.3f);
 				}
-			}
-
-			//meeting point arrow
-			if(unit->getType()->getMeetingPoint()) {
-				Vec2i pos= unit->getMeetingPos();
-				map->clampPos(pos);
-
-				Vec3f arrowTarget= Vec3f(pos.x, map->getCell(pos)->getHeight(), pos.y);
-				renderArrow(unit->getCurrVectorFlat(), arrowTarget, Vec3f(0.f, 0.f, 1.f), 0.3f);
 			}
 		}
 	}
