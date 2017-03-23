f5e09c2a7c1254268327be8b5108f55a353c773d
titiger <titi_linux@megaglest.org>
2017-03-23 01:27:25 +0100

    some more steam stuff

--- mk/steamworks_sdk/sdk/tools/ContentBuilder/run_build_editor.sh
+++ mk/steamworks_sdk/sdk/tools/ContentBuilder/run_build_editor.sh
@@ -3,4 +3,7 @@
 STEAMROOT="$(cd "${0%/*}" && echo $PWD)"
 STEAMCMD=`basename "$0" .sh`
 
+cp -p content/linux_x64/megaglest_editor content/editor_linux_x64/
+cp -p content/linux_x64/glest.ini content/editor_linux_x64/
+
 ./builder_linux/steamcmd.sh +login megaglest_team $1 +run_app_build_http ../scripts/editor_app_linux_611990.vdf +quit
--- mk/steamworks_sdk/sdk/tools/ContentBuilder/run_build_win_editor.sh
+++ mk/steamworks_sdk/sdk/tools/ContentBuilder/run_build_win_editor.sh
@@ -3,4 +3,10 @@
 STEAMROOT="$(cd "${0%/*}" && echo $PWD)"
 STEAMCMD=`basename "$0" .sh`
 
+cp -p content/win_x86/megaglest_editor.exe content/editor_win_x86/
+cp -p content/win_x86/glest.ini content/editor_win_x86/
+
+cp -p content/win_x64/megaglest_editorx64.exe content/editor_win_x64/
+cp -p content/win_x64/glest.ini content/editor_win_x64/
+
 ./builder_linux/steamcmd.sh +login megaglest_team $1 +run_app_build_http ../scripts/editor_app_win_611990.vdf +quit
