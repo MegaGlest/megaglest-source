6daee8e43eddd4c652338546683b8ee622bef598
titiger <titi_linux@megaglest.org>
2017-03-23 01:03:22 +0100

    steam scripts to have the editor in the tools section of steam

---
+++ mk/steamworks_sdk/sdk/tools/ContentBuilder/run_build_editor.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+STEAMROOT="$(cd "${0%/*}" && echo $PWD)"
+STEAMCMD=`basename "$0" .sh`
+
+./builder_linux/steamcmd.sh +login megaglest_team $1 +run_app_build_http ../scripts/editor_app_linux_611990.vdf +quit
