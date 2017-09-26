97a89d0bb0a83787df3f3aaceea1e257e3a5395a
SoftCoder <mark_vejvoda@hotmail.com>
2017-09-25 18:55:35 -0700

    - cleanup makefiles

--- source/steamshim_parent/build.sh
+++ source/steamshim_parent/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Use this script to build MegaGlest using cmake
+# Use this script to build MegaGlest Steam Shim using make
 # ----------------------------------------------------------------------------
 # Written by Mark Vejvoda <mark_vejvoda@hotmail.com>
 # Copyright (c) 2011-2013 Mark Vejvoda under GNU GPL v3.0+
 
 # ----------------------------------------------------------------------------
-rm steamshim
+rm megaglest_shim
+rm megaglest
 #make STEAMWORKS?=/home/softcoder/Code/steamworks_sdk/sdk
 make $@
