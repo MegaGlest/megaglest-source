e1a3e33c7761e4bde46ffbf6343711d002f4a823
pavanvo <anton@pavanvo.me>
2022-08-14 17:41:17 +0400

    style: tabs insted of spaces

--- source/shared_lib/include/util/callback.h
+++ source/shared_lib/include/util/callback.h
@@ -14,58 +14,57 @@ class CallBack {
     
 
 private:
-    function<void()> _onThen = [](){};
-    function<T()> _mainFunc = [](){};
-    T _result;
+	function<void()> _onThen = [](){};
+	function<T()> _mainFunc = [](){};
+	T _result;
     
 public:
-    CallBack(function<T()> func){
-       _mainFunc = func;
-    };
+	CallBack(function<T()> func){
+		_mainFunc = func;
+	};
     
-    ~CallBack() {
-        //printf("CallBack::~CallBack p [%p]\n",this);
-    };
+	~CallBack() {
+		//printf("CallBack::~CallBack p [%p]\n",this);
+	};
     
-    void then(function<void()> onThen){
-        _onThen = onThen;
-    };
+	void then(function<void()> onThen){
+		_onThen = onThen;
+	};
     
-    T getResult(){
-        return _result;
-    }
+	T getResult(){
+		return _result;
+	}
     
-    void run(){
-        _result = _mainFunc();
-        _onThen();
-    };
+	void run(){
+		_result = _mainFunc();
+		_onThen();
+	};
 };
 
 template<>
 class CallBack<void> {
 
 private:
-    function<void()> _onThen = [](){};
-    function<void()> _mainFunc = [](){};
+	function<void()> _onThen = [](){};
+	function<void()> _mainFunc = [](){};
     
 public:
-    CallBack(function<void()> func){
-       _mainFunc = func;
-    };
+	CallBack(function<void()> func){
+		_mainFunc = func;
+	};
     
-    ~CallBack() {
-        //printf("CallBack::~CallBack p [%p]\n",this);
-    };
+	~CallBack() {
+		//printf("CallBack::~CallBack p [%p]\n",this);
+	};
     
-    void then(function<void()> onThen){
-        _onThen = onThen;
-    };
+	void then(function<void()> onThen){
+		_onThen = onThen;
+	};
     
-    void run(){
-        _mainFunc();
-        _onThen();
-    };
-
+	void run(){
+		_mainFunc();
+		_onThen();
+	};
 };
 
 }}
