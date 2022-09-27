5e5aeef4a206b1d8efc825c3f13c6faa2cfa8f00
Jammyjamjamman <jdsherratt3@gmail.com>
2022-09-27 21:54:46 +0100

    Update snapshot to build appimage
    Also add easy-to-use build-appimage.sh script.

---
+++ mk/linux/build-appimage.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+SCRIPTLOC=$(dirname "$0")
+
+if [ -d $SCRIPTLOC/BuildAppImage/AppDir ]; then
+    rm -r $SCRIPTLOC/BuildAppImage/AppDir
+fi
+
+cmake $SCRIPTLOC/../.. -B $SCRIPTLOC/BuildAppImage -DBUILD_MEGAGLEST_MODEL_VIEWER=OFF -DBUILD_MEGAGLEST_MAP_EDITOR=OFF -DBUILD_MEGAGLEST_MODEL_IMPORT_EXPORT_TOOLS=OFF
+cd $SCRIPTLOC/BuildAppImage
+make DESTDIR=AppDir -j$(nproc) install
+strip AppDir/usr/local/bin/megaglest
+
+if [ ! -f AppDir/usr/local/share/applications/megaglest.desktop ]; then 
+    wget -P AppDir/usr/local/share/applications/ https://raw.githubusercontent.com/MegaGlest/megaglest-data/develop/others/desktop/megaglest.desktop;
+fi
+
+wget https://raw.githubusercontent.com/MegaGlest/megaglest-data/develop/others/icons/megaglest.png
+
+if [ ! -f linuxdeploy-x86_64.AppImage ]; then 
+    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20220822-1/linuxdeploy-x86_64.AppImage
+fi
+
+chmod +x linuxdeploy-x86_64.AppImage 
+
+./linuxdeploy-x86_64.AppImage -d AppDir/usr/local/share/applications/megaglest.desktop \
+  --icon-file=megaglest.png \
+   --icon-filename=megaglest --executable AppDir/usr/local/bin/megaglest --appdir AppDir --output appimage
+
+mv MegaGlest*.AppImage ../
