2afefe592c34f1c6d76083bc7c4f74e433d90148
Jammyjamjamman <jdsherratt3@gmail.com>
2023-03-09 00:10:41 +0000

    Fix macos configuration for build

--- mk/macos/build-mg.sh
+++ mk/macos/build-mg.sh
@@ -20,13 +20,35 @@ FORCE_EMBEDDED_LIBS=0
 LUA_FORCED_VERSION=0
 COMPILATION_WITHOUT=0
 
+# Some brew things don't appear to link correctly by themselves.
 if command -v brew &> /dev/null
 then
-    echo "Found brew. Adding links. (Note, this assumes system-wide brew install.)"
-	export PATH="/opt/homebrew/opt/curl/bin:/opt/homebrew/opt/zstd/bin:/opt/homebrew/opt/brotli/bin:$PATH"
-	export LDFLAGS="-L/opt/homebrew/opt/curl/lib -L/opt/homebrew/opt/zstd/lib -L/opt/homebrew/opt/brotli/lib"
-	export CPPFLAGS="-I/opt/homebrew/opt/curl/include -L/opt/homebrew/opt/zstd/include -L/opt/homebrew/opt/brotli/include"
-	export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig:/opt/homebrew/opt/zstd/lib/pkgconfig:/opt/homebrew/opt/brotli/lib/pkgconfig"
+	echo "Found brew. Adding links. (Note, this assumes system-wide brew install.)"
+	if [ -d "$HOMEBREW_PREFIX/opt/curl" ]
+	then
+		for f in $HOMEBREW_PREFIX/opt/{curl,zstd,brotli}
+		do
+			export PATH="$f/bin:$PATH"
+			export LDFLAGS="-L$f/lib $LDFLAGS"
+			export CPPFLAGS="-I$f/include $CPPFLAGS"
+			export PKG_CONFIG_PATH="$f/lib/pkgconfig:$PKG_CONFIG_PATH"
+		done
+	else
+		# Make github happy.
+		# TODO: is there a better way than this?
+		if ! typeset -p HOMEBREW_CELLAR 2> /dev/null | grep -q '^'
+		then
+			export HOMEBREW_CELLAR="/usr/local/Cellar"
+		fi
+
+		for f in $HOMEBREW_CELLAR/{curl,zstd,brotli}/*
+		do
+			export PATH="$f/bin:$PATH"
+			export LDFLAGS="-L$f/lib $LDFLAGS"
+			export CPPFLAGS="-I$f/include $CPPFLAGS"
+			export PKG_CONFIG_PATH="$f/lib/pkgconfig:$PKG_CONFIG_PATH"
+		done
+	fi
 fi
 
 while getopts "c:defhl:mnwxb" option; do
