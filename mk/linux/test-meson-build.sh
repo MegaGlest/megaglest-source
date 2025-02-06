65072e2acbf14124342a6a565d4d7ee815be4f10
andy5995 <arch_stanton5995@proton.me>
2025-02-06 15:30:02 -0600

    Add build script

---
+++ mk/linux/test-meson-build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+HERE="$(readlink -f "$(dirname "$0")")"
+SOURCE_ROOT="$HERE/../.."
+BUILD_DIR="${BUILD_DIR:-$HERE/_build}"
+
+if [ ! -d "$BUILD_DIR" ]; then
+  cd "$SOURCE_ROOT"
+  meson setup "$BUILD_DIR" "$@"
+fi
+
+cd "$HERE"
+
+meson compile -C "$BUILD_DIR"
+
+GAME_BIN="$BUILD_DIR/source/glest_game/megaglest"
+EDITOR_BIN="$BUILD_DIR/source/glest_map_editor/megaglest_map_editor"
+VIEWER_BIN="$BUILD_DIR/source/g3d_viewer/megaglest_g3d_viewer"
+
+for bin in "$GAME_BIN" "$EDITOR_BIN" "$VIEWER_BIN"; do
+  mv -vf "$bin" "$HERE"
+done
