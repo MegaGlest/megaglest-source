cc40fb87e378d5e1c8ed7be70ade742b983cf8e6
andy5995 <arch_stanton5995@proton.me>
2025-03-22 15:06:24 -0500

    Reorder winsock includes

--- source/glest_game/ai/ai_rule.h
+++ source/glest_game/ai/ai_rule.h
@@ -12,10 +12,12 @@
 #ifndef _GLEST_GAME_AIRULE_H_
 #define _GLEST_GAME_AIRULE_H_
 
+// clang-format off
 #ifdef WIN32
-#include <winsock.h>
 #include <winsock2.h>
+#include <winsock.h>
 #endif
+// clang-format on
 
 #include <string>
 
--- source/glest_game/ai/path_finder.h
+++ source/glest_game/ai/path_finder.h
@@ -12,10 +12,12 @@
 #ifndef _GLEST_GAME_PATHFINDER_H_
 #define _GLEST_GAME_PATHFINDER_H_
 
+// clang-format off
 #ifdef WIN32
-#include <winsock.h>
 #include <winsock2.h>
+#include <winsock.h>
 #endif
+// clang-format on
 
 #include <map>
 #include <vector>
